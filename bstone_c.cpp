16206eb79cd7947ecd74a0d4c9bda809392ec34a
bibendovsky <bibendovsky@hotmail.com>
2013-10-03 16:10:42 +0300

    Add wrapper for some non-portable C functions

---
+++ bstone_c.cpp
@@ -0,0 +1,70 @@
+//
+// A wrapper for C platform-dependent functions.
+//
+
+
+#include "bstone_c.h"
+
+#include <algorithm>
+#include <locale>
+
+
+// FIXME
+// Non-"C" locales in MinGW are not supported.
+
+
+namespace bstone {
+
+
+// (static)
+int C::memicmp(
+    const void* buf1,
+    const void* buf2,
+    unsigned int cnt)
+{
+    const char* a = static_cast<const char*>(buf1);
+    const char* b = static_cast<const char*>(buf2);
+
+    std::locale locale("");
+
+    const std::ctype<char>& ctype =
+        std::use_facet<std::ctype<char> >(locale);
+
+    int diff = 0;
+
+    for (unsigned int i = 0; diff == 0 && i < cnt; ++i) {
+        diff = static_cast<unsigned char>(ctype.tolower(a[i])) -
+            static_cast<unsigned char>(ctype.tolower(b[i]));
+    }
+
+    return diff;
+}
+
+// (static)
+int C::stricmp(
+    const char* str1,
+    const char* str2)
+{
+    std::locale locale("");
+
+    const std::ctype<char>& ctype =
+        std::use_facet<std::ctype<char> >(locale);
+
+    int diff = 0;
+
+    for (unsigned int i = 0; diff == 0; ++i) {
+        char a = str1[i];
+        char b = str2[i];
+
+        diff = static_cast<unsigned char>(ctype.tolower(a)) -
+            static_cast<unsigned char>(ctype.tolower(b));
+
+        if (a == '\0' || b == '\0')
+            break;
+    }
+
+    return diff;
+}
+
+
+} // namespace bstone
---
+++ bstone_c.h
@@ -0,0 +1,84 @@
+//
+// A wrapper for C platform-dependent functions.
+//
+
+
+#ifndef BSTONE_C_H
+#define BSTONE_C_H
+
+
+#include <sstream>
+
+
+namespace bstone {
+
+
+// A wrapper for C platform-dependent functions.
+class C {
+public:
+    template<class T>
+    static char* xitoa(
+        T value,
+        char* str,
+        int radix)
+    {
+        if (str == NULL)
+            return NULL;
+
+        str[0] = '\0';
+        std::ios::fmtflags flags;
+
+        std::ostringstream iss;
+
+        switch (radix) {
+        case 8:
+            flags = std::ios::oct;
+            break;
+
+        case 10:
+            flags = std::ios::dec;
+            break;
+
+        case 16:
+            flags = std::ios::hex;
+            break;
+
+        default:
+            flags = 0;
+            break;
+        }
+
+        if (flags != 0) {
+
+            iss.setf(flags, std::ios::oct | std::ios::dec | std::ios::hex);
+            iss << value;
+
+            if (iss) {
+                std::string value_str = iss.str();
+                std::string::traits_type::copy(
+                    str,
+                    value_str.c_str(),
+                    value_str.size());
+                str[value_str.size()] = '\0';
+            }
+        }
+
+        return str;
+    }
+
+    static int memicmp(
+        const void* buf1,
+        const void* buf2,
+        unsigned int cnt);
+
+    static int stricmp(
+        const char* str1,
+        const char* str2);
+}; // class C
+
+
+} // namespace bstone
+
+
+#endif // BSTONE_C_H
+
