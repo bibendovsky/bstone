62d63bcc5188acf587e267a2895f5f30a36acf37
bibendovsky <bibendovsky@hotmail.com>
2013-10-12 13:15:04 +0300

    Fix pointer to Uint16 (and vise versa) conversion

--- stub.cpp
+++ stub.cpp
@@ -1,18 +1,13 @@
 #include "3d_def.h"
 
 
-const int OLD_OBJTYPE_SIZE = 70;
-const int OLD_STATOBJ_T_SIZE = 12;
-const int OLD_DOOROBJ_T_SIZE = 16;
-
-
 //
 // Offsets of variables in data segment.
 //
 
-const int OBJLIST_OFFSET = 0xFFFF - (MAXACTORS * OLD_OBJTYPE_SIZE);
-const int STATOBJLIST_OFFSET = 0xFFFF - (MAXSTATS * OLD_STATOBJ_T_SIZE);
-const int DOOROBJLIST_OFFSET = 0xFFFF - (MAXDOORS * OLD_DOOROBJ_T_SIZE);
+const int OBJLIST_OFFSET = 0xFFFF - MAXACTORS;
+const int STATOBJLIST_OFFSET = 0xFFFF - MAXSTATS;
+const int DOOROBJLIST_OFFSET = 0xFFFF - MAXDOORS;
 
 
 void ogl_update_screen();
@@ -65,80 +60,86 @@ Sint16 VL_VideoID ()
     return 5;
 }
 
-objtype* ui16_to_actor(Uint16 value)
+objtype* ui16_to_actor(
+    Uint16 value)
 {
-    int offset = value - OBJLIST_OFFSET;
+    int index = value - OBJLIST_OFFSET;
 
-    if (offset >= 0) {
-        int index = offset / OLD_OBJTYPE_SIZE;
-        assert((offset % OLD_OBJTYPE_SIZE) == 0);
-        return &objlist[index];
-    }
+    if (index < 0)
+        return NULL;
 
-    return NULL;
+    if (index >= MAXACTORS)
+        return NULL;
+
+    return &objlist[index];
 }
 
-Uint16 actor_to_ui16(const objtype* actor)
+Uint16 actor_to_ui16(
+    const objtype* actor)
 {
     ptrdiff_t index = actor - objlist;
 
-    if (index >= 0) {
-        ptrdiff_t offset = OBJLIST_OFFSET + (index * OLD_OBJTYPE_SIZE);
-        assert(offset <= 0xFFFF);
-        return static_cast<Uint16>(offset);
-    }
+    if (index < 0)
+        return 0;
 
-    return 0;
+    if (index >= MAXACTORS)
+        return 0;
+
+    return static_cast<Uint16>(index + OBJLIST_OFFSET);
 }
 
-statobj_t* ui16_to_static_object(Uint16 value)
+statobj_t* ui16_to_static_object(
+    Uint16 value)
 {
-    int offset = value - STATOBJLIST_OFFSET;
+    int index = value - STATOBJLIST_OFFSET;
 
-    if (offset >= 0) {
-        int index = offset / OLD_STATOBJ_T_SIZE;
-        assert((offset % OLD_STATOBJ_T_SIZE) == 0);
-        return &statobjlist[index];
-    }
+    if (index < 0)
+        return NULL;
 
-    return NULL;
+    if (index >= MAXSTATS)
+        return NULL;
+
+    return &statobjlist[index];
 }
 
-Uint16 static_object_to_ui16(const statobj_t* static_object)
+Uint16 static_object_to_ui16(
+    const statobj_t* static_object)
 {
     ptrdiff_t index = static_object - statobjlist;
 
-    if (index >= 0) {
-        ptrdiff_t offset = STATOBJLIST_OFFSET + (index * OLD_STATOBJ_T_SIZE);
-        assert(offset <= 0xFFFF);
-        return static_cast<Uint16>(offset);
-    }
+    if (index < 0)
+        return NULL;
 
-    return 0;
+    if (index >= MAXSTATS)
+        return NULL;
+
+    return static_cast<Uint16>(index + STATOBJLIST_OFFSET);
 }
 
-doorobj_t* ui16_to_door_object(Uint16 value)
+doorobj_t* ui16_to_door_object(
+    Uint16 value)
 {
-    int offset = value - DOOROBJLIST_OFFSET;
+    int index = value - DOOROBJLIST_OFFSET;
 
-    if (offset >= 0) {
-        int index = offset / OLD_DOOROBJ_T_SIZE;
-        assert((offset % OLD_DOOROBJ_T_SIZE) == 0);
-        return &doorobjlist[index];
-    }
+    if (index < 0)
+        return NULL;
 
-    return NULL;
+    if (index >= MAXDOORS)
+        return NULL;
+
+    return &doorobjlist[index];
 }
 
-Uint16 door_object_to_ui16(const doorobj_t* door_object)
+Uint16 door_object_to_ui16(
+    const doorobj_t* door_object)
 {
     ptrdiff_t index = door_object - doorobjlist;
 
-    if (index >= 0) {
-        ptrdiff_t offset = DOOROBJLIST_OFFSET + (index * OLD_DOOROBJ_T_SIZE);
-        assert(offset <= 0xFFFF);
-        return static_cast<Uint16>(offset);
-    }
+    if (index < 0)
+        return 0;
 
-    return 0;
+    if (index >= MAXDOORS)
+        return 0;
+
+    return static_cast<Uint16>(index + DOOROBJLIST_OFFSET);
 }
