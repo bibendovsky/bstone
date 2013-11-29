79fc33e26d3592d69b19df6edc71b7011d45d16a
bibendovsky <bibendovsky@hotmail.com>
2013-11-29 19:51:06 +0200

    Add license plate to every source code file

--- bstone_format_string.cpp
+++ bstone_format_string.cpp
@@ -1,3 +1,26 @@
+/* ==============================================================
+bstone: A source port of Blake Stone: Planet Strike
+
+Copyright (c) 1992-2013 Apogee Entertainment, LLC
+Copyright (c) 2013 Boris Bendovsky (bibendovsky@hotmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the
+Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+============================================================== */
+
+
 //
 // A wrapper for std::ostringstream to produce a formatted string.
 //
