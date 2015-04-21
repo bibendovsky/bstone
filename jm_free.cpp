75a380db6d1f32235f5662c17f494ba90ccf51a6
bibendovsky <bibendovsky@hotmail.com>
2015-04-21 13:55:59 +0300

    Merge AOG and PS code in jm_free

--- jm_free.cpp
+++ jm_free.cpp
@@ -1031,45 +1031,44 @@ void PreDemo()
     VL_SetPaletteIntensity(0, 255, vgapal, 0);
 
     if (!(gamestate.flags & GS_NOWAIT)) {
-#if defined(BSTONE_AOG) && GAME_VERSION != SHAREWARE_VERSION
-// ---------------------
-// Anti-piracy screen
-// ---------------------
-        // Cache pic
-        //
-        CA_CacheScreen(PIRACYPIC);
+        if (::is_aog_sw()) {
+            // ---------------------
+            // Anti-piracy screen
+            // ---------------------
 
-        // Cache and set palette.  AND  Fade it in!
-        //
-        CA_CacheGrChunk(PIRACYPALETTE);
-        VL_SetPalette(0, 256, static_cast<const Uint8*>(grsegs[PIRACYPALETTE]));
-        VL_SetPaletteIntensity(0, 255, static_cast<const Uint8*>(grsegs[PIRACYPALETTE]), 0);
-        VW_UpdateScreen();
+            // Cache pic
+            //
+            CA_CacheScreen(PIRACYPIC);
 
-        VL_FadeOut(0, 255, 0, 0, 25, 20);
-        VL_FadeIn(0, 255, static_cast<const Uint8*>(grsegs[PIRACYPALETTE]), 30);
+            // Cache and set palette.  AND  Fade it in!
+            //
+            CA_CacheGrChunk(PIRACYPALETTE);
+            VL_SetPalette(0, 256, static_cast<const Uint8*>(grsegs[PIRACYPALETTE]));
+            VL_SetPaletteIntensity(0, 255, static_cast<const Uint8*>(grsegs[PIRACYPALETTE]), 0);
+            VW_UpdateScreen();
 
-        // Wait a little
-        //
-        IN_UserInput(TickBase * 20);
+            VL_FadeOut(0, 255, 0, 0, 25, 20);
+            VL_FadeIn(0, 255, static_cast<const Uint8*>(grsegs[PIRACYPALETTE]), 30);
 
-        // Free palette
-        //
-        UNCACHEGRCHUNK(PIRACYPALETTE);
+            // Wait a little
+            //
+            IN_UserInput(TickBase * 20);
 
-        VL_FadeOut(0, 255, 0, 0, 25, 20);
-        VW_FadeOut();
+            // Free palette
+            //
+            UNCACHEGRCHUNK(PIRACYPALETTE);
 
-        // Cleanup screen for upcoming SetPalette call
-        //
-        {
+            VL_FadeOut(0, 255, 0, 0, 25, 20);
+            VW_FadeOut();
+
+            // Cleanup screen for upcoming SetPalette call
+            //
             Uint16 old_bufferofs = bufferofs;
 
             bufferofs = displayofs;
             VL_Bar(0, 0, 320, 200, 0);
             bufferofs = old_bufferofs;
         }
-#endif // BSTONE_AOG
 
 // ---------------------
 // Apogee presents
@@ -1090,11 +1089,11 @@ void PreDemo()
         VL_SetPalette(0, 256, static_cast<const Uint8*>(grsegs[APOGEEPALETTE]));
         VL_SetPaletteIntensity(0, 255, static_cast<const Uint8*>(grsegs[APOGEEPALETTE]), 0);
         VW_UpdateScreen();
-#ifdef BSTONE_AOG
-        VL_FadeOut(0, 255, 0, 0, 0, 20);
-#else
-        VL_FadeOut(0, 255, 25, 29, 53, 20);
-#endif // BSTONE_AOG
+        if (!::is_ps) {
+            VL_FadeOut(0, 255, 0, 0, 0, 20);
+        } else {
+            VL_FadeOut(0, 255, 25, 29, 53, 20);
+        }
         VL_FadeIn(0, 255, static_cast<const Uint8*>(grsegs[APOGEEPALETTE]), 30);
 
         // Wait for end of fanfare
@@ -1116,10 +1115,10 @@ void PreDemo()
         delete [] audiosegs[STARTMUSIC + APOGFNFM_MUS];
         audiosegs[STARTMUSIC + APOGFNFM_MUS] = NULL;
 
-#ifdef BSTONE_PS
-        // Do A Blue Flash!
-        VL_FadeOut(0, 255, 25, 29, 53, 20);
-#endif // BSTONE_PS
+        if (::is_ps()) {
+            // Do A Blue Flash!
+            VL_FadeOut(0, 255, 25, 29, 53, 20);
+        }
 
         VL_FadeOut(0, 255, 0, 0, 0, 30);
 
@@ -1128,13 +1127,8 @@ void PreDemo()
 // ---------------------
         // Load and start music
         //
-#ifdef BSTONE_AOG
-        CA_CacheAudioChunk(STARTMUSIC + GOLDA_MUS);
-        ::SD_StartMusic(GOLDA_MUS);
-#else
         CA_CacheAudioChunk(STARTMUSIC + TITLE_LOOP_MUSIC);
         ::SD_StartMusic(TITLE_LOOP_MUSIC);
-#endif // BSTONE_AOG
 
         // Show JAM logo
         //
@@ -1163,13 +1157,8 @@ void PreDemo()
                     ClearMemory();
                     DebugOk = 1;
 
-#ifdef BSTONE_AOG
-                    CA_CacheAudioChunk(STARTMUSIC + GOLDA_MUS);
-                    ::SD_StartMusic(GOLDA_MUS);
-#else
                     CA_CacheAudioChunk(STARTMUSIC + TITLE_LOOP_MUSIC);
                     ::SD_StartMusic(TITLE_LOOP_MUSIC);
-#endif // BSTONE_AOG
                 }
             }
         }
