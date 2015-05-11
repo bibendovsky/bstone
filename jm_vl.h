759a7fa6fa6feb48500962a9c4d2ca8276c916a5
bibendovsky <bibendovsky@hotmail.com>
2015-05-11 17:27:40 +0300

    Update license plate

--- jm_vl.h
+++ jm_vl.h
@@ -1,8 +1,9 @@
-/* ==============================================================
-bstone: A source port of Blake Stone: Planet Strike
+/*
+BStone: A Source port of
+Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
 
 Copyright (c) 1992-2013 Apogee Entertainment, LLC
-Copyright (c) 2013 Boris Bendovsky (bibendovsky@hotmail.com)
+Copyright (c) 2013-2015 Boris I. Bendovsky (bibendovsky@hotmail.com)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -18,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the
 Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-============================================================== */
+*/
 
 
 #ifndef BSTONE_JM_VL_INCLUDED
