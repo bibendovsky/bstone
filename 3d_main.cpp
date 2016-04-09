177158bcebf9d943abd8a428a9c75c64697b19af
bibendovsky <bibendovsky@hotmail.com>
2016-04-09 14:42:06 +0300

    Update version

--- 3d_main.cpp
+++ 3d_main.cpp
@@ -9099,7 +9099,7 @@ void sys_default_sleep_for()
 
 const std::string& get_version_string()
 {
-    static const std::string version = "1.1.5";
+    static const std::string version = "1.1.7";
     return version;
 }
 
