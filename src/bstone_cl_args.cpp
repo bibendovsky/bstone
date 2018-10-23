f14ed7cb35df9694bb8f45d2d9719ac19a915823
bibendovsky <bibendovsky@hotmail.com>
2018-10-23 12:01:48 +0300

    Revise CL arguments

--- src/bstone_cl_args.cpp
+++ src/bstone_cl_args.cpp
@@ -25,239 +25,151 @@ Free Software Foundation, Inc.,
 #include "bstone_cl_args.h"
 
 
-namespace bstone {
+namespace bstone
+{
 
 
 ClArgs::ClArgs() :
-        args_(),
-        lc_args_()
+	args_{},
+	lc_args_{}
 {
-}
-
-ClArgs::ClArgs(
-    const ClArgs& that) :
-        args_(that.args_),
-        lc_args_(that.lc_args_)
-{
-}
-
-ClArgs::~ClArgs()
-{
-}
-
-ClArgs& ClArgs::operator=(
-    const ClArgs& that)
-{
-    if (&that != this) {
-        args_ = that.args_;
-        lc_args_ = that.lc_args_;
-    }
-
-    return *this;
 }
 
 const std::string& ClArgs::operator[](
-    int index) const
+	const int index) const
 {
-    return get_argument(index);
+	return get_argument(index);
 }
 
 void ClArgs::initialize(
-    int argc,
-    char* const* argv)
+	const int argc,
+	char* const* argv)
 {
-    unintialize();
+	unintialize();
 
-    args_.resize(argc);
-    lc_args_.resize(argc);
+	args_.resize(argc);
+	lc_args_.resize(argc);
 
-    for (int i = 0; i < argc; ++i) {
-        args_[i] = argv[i];
-        lc_args_[i] = StringHelper::to_lower(args_[i]);
-    }
+	for (int i = 0; i < argc; ++i)
+	{
+		args_[i] = argv[i];
+		lc_args_[i] = StringHelper::to_lower(args_[i]);
+	}
 }
 
 void ClArgs::unintialize()
 {
-    StringList().swap(args_);
-    StringList().swap(lc_args_);
+	args_ = {};
+	lc_args_ = {};
 }
 
 bool ClArgs::has_option(
-    const std::string& option_name) const
+	const std::string& option_name) const
 {
-    return find_option(option_name) >= 0;
+	return find_option(option_name) >= 0;
 }
 
 int ClArgs::find_option(
-    const std::string& option_name) const
+	const std::string& option_name) const
 {
-    if (option_name.empty()) {
-        return -1;
-    }
+	if (option_name.empty())
+	{
+		return -1;
+	}
 
-    auto lc_name = StringHelper::to_lower(option_name);
+	const auto& lc_name = StringHelper::to_lower(option_name);
+	const auto arg_count = static_cast<int>(args_.size());
 
-    for (int i = 1; i < get_count(); ++i) {
-        const auto& arg = args_[i];
+	for (int i = 0; i < arg_count; ++i)
+	{
+		const auto& arg = args_[arg_count - i - 1];
 
-        if (arg.size() != (2 + lc_name.size())) {
-            continue;
-        }
+		if (arg.size() != (2 + lc_name.size()))
+		{
+			continue;
+		}
 
-        if (arg.compare(0, 2, "--") != 0) {
-            continue;
-        }
+		if (arg.compare(0, 2, "--") != 0)
+		{
+			continue;
+		}
 
-        if (arg.compare(2, lc_name.size(), lc_name) == 0) {
-            return i;
-        }
-    }
+		if (arg.compare(2, lc_name.size(), lc_name) == 0)
+		{
+			return i;
+		}
+	}
 
-    return -1;
+	return -1;
 }
 
 int ClArgs::find_argument(
-    const std::string& name) const
+	const std::string& name) const
 {
-    if (name.empty()) {
-        return -1;
-    }
+	if (name.empty())
+	{
+		return -1;
+	}
 
-    auto lc_name = StringHelper::to_lower(name);
+	const auto& lc_name = StringHelper::to_lower(name);
 
-    for (int i = 1; i < get_count(); ++i) {
-        if (lc_name == args_[i]) {
-            return i;
-        }
-    }
+	for (int i = 1; i < get_count(); ++i)
+	{
+		if (lc_name == args_[i])
+		{
+			return i;
+		}
+	}
 
-    return -1;
-}
-
-int ClArgs::check_argument(
-    const char* const list[])
-{
-    if (args_.size() <= 1) {
-        return -1;
-    }
-
-    if (!list) {
-        return -1;
-    }
-
-    std::string item;
-
-    for (auto i = lc_args_.cbegin() + 1; i != lc_args_.cend(); ++i) {
-        size_t arg_index = 0;
-
-        if (i->size() >= 2 && i->compare(0, 2, "--") == 0) {
-            arg_index = 2;
-        }
-
-        item = StringHelper::to_lower(i->substr(arg_index));
-
-        if (item.empty()) {
-            continue;
-        }
-
-        for (int j = 0; list[j]; ++j) {
-            auto list_item = list[j];
-
-            if (item == list_item) {
-                return j;
-            }
-        }
-    }
-
-    return -1;
-}
-
-int ClArgs::check_argument(
-    const char* const list[],
-    std::string& found_argument)
-{
-    found_argument.clear();
-
-    if (args_.size () <= 1) {
-        return -1;
-    }
-
-    if (!list) {
-        return -1;
-    }
-
-    std::string item;
-
-    for (auto i = lc_args_.cbegin() + 1; i != lc_args_.cend(); ++i) {
-        size_t arg_index = 0;
-
-        if (i->size() >= 2 && i->compare(0, 2, "--") == 0) {
-            arg_index = 2;
-        }
-
-        item = StringHelper::to_lower(i->substr(arg_index));
-
-        if (item.empty()) {
-            continue;
-        }
-
-        for (int j = 0; list[j]; ++j) {
-            auto list_item = list[j];
-
-            if (item == list_item) {
-                found_argument = item;
-                return j;
-            }
-        }
-    }
-
-    return -1;
+	return -1;
 }
 
 int ClArgs::get_count() const
 {
-    return static_cast<int>(args_.size());
+	return static_cast<int>(args_.size());
 }
 
 const std::string& ClArgs::get_argument(
-    int index) const
+	const int index) const
 {
-    if (index < 0 || index >= get_count()) {
-        return StringHelper::get_empty();
-    }
+	if (index < 0 || index >= get_count())
+	{
+		return StringHelper::get_empty();
+	}
 
-    return args_[index];
+	return args_[index];
 }
 
 const std::string& ClArgs::get_option_value(
-    const std::string& option_name) const
+	const std::string& option_name) const
 {
-    auto option_index = find_option(option_name);
+	auto option_index = find_option(option_name);
 
-    if (option_index >= 0) {
-        ++option_index;
-    }
+	if (option_index >= 0)
+	{
+		++option_index;
+	}
 
-    return get_argument(option_index);
+	return get_argument(option_index);
 }
 
 void ClArgs::get_option_values(
-    const std::string& option_name,
-    std::string& value1,
-    std::string& value2) const
+	const std::string& option_name,
+	std::string& value1,
+	std::string& value2) const
 {
-    value1.clear();
-    value2.clear();
+	value1.clear();
+	value2.clear();
 
-    auto option_index = find_option(option_name);
+	const auto option_index = find_option(option_name);
 
-    if (option_index < 0) {
-        return;
-    }
+	if (option_index < 0)
+	{
+		return;
+	}
 
-    value1 = get_argument(option_index + 1);
-    value2 = get_argument(option_index + 2);
+	value1 = get_argument(option_index + 1);
+	value2 = get_argument(option_index + 2);
 }
 
 
