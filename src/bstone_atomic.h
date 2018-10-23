a8404c9ba849b8512ed1469fcf4f1693b8818964
bibendovsky <bibendovsky@hotmail.com>
2018-10-23 15:12:35 +0300

    Revise atomic

--- src/bstone_atomic.h
+++ src/bstone_atomic.h
@@ -32,79 +32,86 @@ Free Software Foundation, Inc.,
 
 
 #include <atomic>
+#include <memory>
 #include <type_traits>
 
 
-namespace bstone {
+namespace bstone
+{
 
 
 template<typename T>
-class Atomic {
+class Atomic
+{
 public:
-    Atomic() :
-            value_(T())
-    {
-    }
+	Atomic()
+		:
+		value_{T{}}
+	{
+	}
 
-    Atomic(
-        const T& value) :
-            value_(value)
-    {
-    }
+	Atomic(
+		const T& value)
+		:
+		value_{value}
+	{
+	}
 
-    Atomic(
-        const Atomic& that) :
-            value_(that.value_.load())
-    {
-    }
+	Atomic(
+		const Atomic& that)
+		:
+		value_{that.value_.load()}
+	{
+	}
 
-    Atomic& operator=(
-        const T& value)
-    {
-        value_ = value;
-        return *this;
-    }
+	Atomic& operator=(
+		const T& value)
+	{
+		value_ = value;
+		return *this;
+	}
 
-    Atomic& operator=(
-        const Atomic& that)
-    {
-        if (&that != this) {
-            value_ = that.value_.load();
-        }
+	Atomic& operator=(
+		const Atomic& that)
+	{
+		if (std::addressof(that) != this)
+		{
+			value_ = that.value_.load();
+		}
 
-        return *this;
-    }
+		return *this;
+	}
 
-    ~Atomic()
-    {
-    }
+	~Atomic()
+	{
+	}
 
-    operator T() const
-    {
-        return value_;
-    }
+	operator T() const
+	{
+		return value_;
+	}
 
-    Atomic& operator-=(
-        typename std::enable_if<std::is_integral<T>::value, const T&>::type value)
-    {
-        value_ -= value;
-        return *this;
-    }
+	Atomic& operator-=(
+		typename std::enable_if<std::is_integral<T>::value, const T&>::type value)
+	{
+		value_ -= value;
+		return *this;
+	}
 
-    Atomic& operator+=(
-        typename std::enable_if<std::is_integral<T>::value, const T&>::type value)
-    {
-        value_ += value;
-        return *this;
-    }
+	Atomic& operator+=(
+		typename std::enable_if<std::is_integral<T>::value, const T&>::type value)
+	{
+		value_ += value;
+		return *this;
+	}
 
 
 private:
-    std::atomic<T> value_;
+	std::atomic<T> value_;
 }; // Atomic
 
 
 } // bstone
 
 
-#endif // BSTONE_ATOMIC_INCLUDED
+#endif // !BSTONE_ATOMIC_INCLUDED
