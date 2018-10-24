56e3f79de37546f6c9a792b6c14b99a83a0c470c
bibendovsky <bibendovsky@hotmail.com>
2018-10-24 11:55:19 +0300

    Revise sprite

--- src/bstone_sprite.cpp
+++ src/bstone_sprite.cpp
@@ -4,7 +4,7 @@
 
 
 #include "bstone_sprite.h"
-#include <cstdint>
+#include <stdexcept>
 #include <memory>
 #include <utility>
 #include "bstone_endian.h"
@@ -14,40 +14,51 @@ namespace bstone
 {
 
 
-Sprite::Sprite() :
-        left_{},
-        right_{},
-        top_{},
-        bottom_{},
-        image_{}
+Sprite::Sprite()
+	:
+	left_{},
+	right_{},
+	top_{},
+	bottom_{},
+	image_{}
 {
 }
 
 Sprite::Sprite(
-    const Sprite& that) :
-        left_{ that.left_ },
-        right_{ that.right_ },
-        top_{ that.top_ },
-        bottom_{ that.bottom_ },
-        image_{ that.image_ }
+	const Sprite& rhs)
+	:
+	left_{rhs.left_},
+	right_{rhs.right_},
+	top_{rhs.top_},
+	bottom_{rhs.bottom_},
+	image_{rhs.image_}
 {
 }
 
 Sprite::Sprite(
-    Sprite&& that) :
-        left_{ std::move(that.left_) },
-        right_{ std::move(that.right_) },
-        top_{ std::move(that.top_) },
-        bottom_{ std::move(that.bottom_) },
-        image_{ std::move(that.image_) }
+	Sprite&& rhs)
+	:
+	left_{std::move(rhs.left_)},
+	right_{std::move(rhs.right_)},
+	top_{std::move(rhs.top_)},
+	bottom_{std::move(rhs.bottom_)},
+	image_{std::move(rhs.image_)}
 {
 }
 
 Sprite& Sprite::operator=(
-    Sprite that)
+	const Sprite& rhs)
 {
-    swap(*this, that);
-    return *this;
+	if (std::addressof(rhs) != this)
+	{
+		left_ = rhs.left_;
+		right_ = rhs.right_;
+		top_ = rhs.top_;
+		bottom_ = rhs.bottom_;
+		image_ = rhs.image_;
+	}
+
+	return *this;
 }
 
 Sprite::~Sprite()
@@ -55,166 +66,152 @@ Sprite::~Sprite()
 }
 
 void Sprite::initialize(
-    const void* raw_data)
+	const void* raw_data)
 {
-    uninitialize();
+	uninitialize();
 
-    if (!raw_data)
-    {
-        throw "No raw data.";
-    }
+	if (!raw_data)
+	{
+		throw std::runtime_error{"No raw data."};
+	}
 
-    const auto values_16 = static_cast<const uint16_t*>(raw_data);
-    const auto values_8 = static_cast<const uint8_t*>(raw_data);
+	const auto values_16 = static_cast<const std::uint16_t*>(raw_data);
+	const auto values_8 = static_cast<const std::uint8_t*>(raw_data);
 
 	const auto& endian = bstone::Endian{};
 
-    const int left = endian.little(values_16[0]);
-    const int right = endian.little(values_16[1]);
+	const int left = endian.little(values_16[0]);
+	const int right = endian.little(values_16[1]);
 
-    if (left > right || left >= side || right >= side)
-    {
-        throw "Invalid edge values.";
-    }
+	if (left > right || left >= side || right >= side)
+	{
+		throw std::runtime_error{"Invalid edge values."};
+	}
 
-    const auto commands_offsets = &values_16[2];
+	const auto commands_offsets = &values_16[2];
 
 
-    // Find out vertical bounds.
-    //
-    int top = -1;
-    int bottom = -1;
+	// Find out vertical bounds.
+	//
+	auto top = -1;
+	auto bottom = -1;
 
-    for (int i = left; i <= right; ++i)
-    {
-        const int commands_offset = endian.little(commands_offsets[i - left]);
-        const auto commands = reinterpret_cast<const uint16_t*>(&values_8[commands_offset]);
+	for (int i = left; i <= right; ++i)
+	{
+		const auto commands_offset = endian.little(commands_offsets[i - left]);
+		const auto commands = reinterpret_cast<const std::uint16_t*>(&values_8[commands_offset]);
 
-        int iCommand = 0;
+		auto iCommand = 0;
 
-        int end = endian.little(commands[iCommand++]) / 2;
+		auto end = endian.little(commands[iCommand++]) / 2;
 
-        while (end != 0)
-        {
-            const int pixels_offset = endian.little(commands[iCommand++]);
-            const int start = endian.little(commands[iCommand++]) / 2;
+		while (end != 0)
+		{
+			const auto pixels_offset = static_cast<int>(endian.little(commands[iCommand++]));
+			const auto start = endian.little(commands[iCommand++]) / 2;
 
-            if (top < 0 || start < top)
-            {
-                top = start;
-            }
+			if (top < 0 || start < top)
+			{
+				top = start;
+			}
 
-            if (bottom < 0 || end > bottom)
-            {
-                bottom = end;
-            }
+			if (bottom < 0 || end > bottom)
+			{
+				bottom = end;
+			}
 
-            end = endian.little(commands[iCommand++]) / 2;
-        }
-    }
+			end = endian.little(commands[iCommand++]) / 2;
+		}
+	}
 
-    // Build the image.
-    //
+	// Build the image.
+	//
 
-    const auto width = (right - left) + 1;
-    const auto height = (bottom - top) + 1;
-    const auto area = width * height;
+	const auto width = (right - left) + 1;
+	const auto height = (bottom - top) + 1;
+	const auto area = width * height;
 
-    image_.resize(area, -1);
+	image_.resize(area, -1);
 
-    for (int i = 0; i < width; ++i)
-    {
-        const int commands_offset = endian.little(commands_offsets[i]);
-        const auto commands = reinterpret_cast<const uint16_t*>(&values_8[commands_offset]);
+	for (int i = 0; i < width; ++i)
+	{
+		const auto commands_offset = static_cast<int>(endian.little(commands_offsets[i]));
+		const auto commands = reinterpret_cast<const uint16_t*>(&values_8[commands_offset]);
 
-        int iCommand = 0;
+		auto iCommand = 0;
 
-        int end = endian.little(commands[iCommand++]) / 2;
+		auto end = endian.little(commands[iCommand++]) / 2;
 
-        while (end != 0)
-        {
-            int pixels_offset = endian.little(commands[iCommand++]);
-            const int start = endian.little(commands[iCommand++]) / 2;
-            const auto count = end - start;
+		while (end != 0)
+		{
+			auto pixels_offset = static_cast<int>(endian.little(commands[iCommand++]));
+			const auto start = static_cast<int>(endian.little(commands[iCommand++]) / 2);
+			const auto count = end - start;
 
-            pixels_offset += start;
-            pixels_offset %= 0x10000;
+			pixels_offset += start;
+			pixels_offset %= 0x10000;
 
-            const auto src_pixels = &values_8[pixels_offset];
-            auto dst_pixels = &image_[(i * height) + (start - top)];
+			const auto src_pixels = &values_8[pixels_offset];
+			auto dst_pixels = &image_[(i * height) + (start - top)];
 
-            std::uninitialized_copy_n(
-                src_pixels,
-                count,
-                dst_pixels);
+			std::uninitialized_copy_n(src_pixels, count, dst_pixels);
 
-            end = endian.little(commands[iCommand++]) / 2;
-        }
-    }
+			end = endian.little(commands[iCommand++]) / 2;
+		}
+	}
 
-    left_ = left;
-    right_ = right;
-    top_ = top;
-    bottom_ = bottom;
+	left_ = left;
+	right_ = right;
+	top_ = top;
+	bottom_ = bottom;
 }
 
 void Sprite::uninitialize()
 {
-    left_ = 0;
-    right_ = 0;
-    Image{}.swap(image_);
+	left_ = 0;
+	right_ = 0;
+	image_ = {};
 }
 
 bool Sprite::is_initialized() const
 {
-    return !image_.empty();
+	return !image_.empty();
 }
 
 int Sprite::get_left() const
 {
-    return left_;
+	return left_;
 }
 
 int Sprite::get_right() const
 {
-    return right_;
+	return right_;
 }
 
 int Sprite::get_top() const
 {
-    return top_;
+	return top_;
 }
 
 int Sprite::get_bottom() const
 {
-    return bottom_;
+	return bottom_;
 }
 
 int Sprite::get_width() const
 {
-    return (right_ - left_) + 1;
+	return (right_ - left_) + 1;
 }
 
 int Sprite::get_height() const
 {
-    return (bottom_ - top_) + 1;
+	return (bottom_ - top_) + 1;
 }
 
 const short* Sprite::get_column(
-    const int index) const
+	const int index) const
 {
-    return &image_[index * get_height()];
-}
-
-void Sprite::swap(
-    Sprite& a,
-    Sprite& b)
-{
-    std::swap(a.left_, b.left_);
-    std::swap(a.right_, b.right_);
-    std::swap(a.top_, b.top_);
-    std::swap(a.bottom_, b.bottom_);
-    std::swap(a.image_, b.image_);
+	return &image_[index * get_height()];
 }
 
 
--- src/bstone_sprite.h
+++ src/bstone_sprite.h
@@ -18,72 +18,68 @@ namespace bstone
 class Sprite
 {
 public:
-    // Sprite dimension.
-    static constexpr int side = 64;
+	// Sprite dimension.
+	static constexpr auto side = 64;
 
 
-    Sprite();
+	Sprite();
 
-    Sprite(
-        const Sprite& that);
+	Sprite(
+		const Sprite& rhs);
 
-    Sprite(
-        Sprite&& that);
+	Sprite(
+		Sprite&& rhs);
 
-    Sprite& operator=(
-        Sprite that);
+	Sprite& operator=(
+		const Sprite& rhs);
 
-    ~Sprite();
-
-
-    void initialize(
-        const void* raw_data);
-
-    void uninitialize();
+	~Sprite();
 
 
-    // Returns true if sprite is initialized.
-    bool is_initialized() const;
+	void initialize(
+		const void* raw_data);
 
-    // Return an offset by x of the left edge.
-    int get_left() const;
-
-    // Return an offset by x of the right edge.
-    int get_right() const;
-
-    // Return an offset by y of the top edge.
-    int get_top() const;
-
-    // Return an offset by y of the bottm edge.
-    int get_bottom() const;
-
-    // Returns a width of the sprite.
-    int get_width() const;
-
-    // Returns a height of the sprite.
-    int get_height() const;
-
-    // Returns a pointer to the column data.
-    //
-    // Note: Negative value means transparency.
-    const short* get_column(
-        const int index) const;
+	void uninitialize();
 
 
-    static void swap(
-        Sprite& a,
-        Sprite& b);
+	// Returns true if sprite is initialized.
+	bool is_initialized() const;
+
+	// Return an offset by x of the left edge.
+	int get_left() const;
+
+	// Return an offset by x of the right edge.
+	int get_right() const;
+
+	// Return an offset by y of the top edge.
+	int get_top() const;
+
+	// Return an offset by y of the bottm edge.
+	int get_bottom() const;
+
+	// Returns a width of the sprite.
+	int get_width() const;
+
+	// Returns a height of the sprite.
+	int get_height() const;
+
+	// Returns a pointer to the column data.
+	//
+	// Note: Negative value means transparency.
+	const short* get_column(
+		const int index) const;
 
 
 private:
-    using Image = std::vector<int16_t>;
+	using Image = std::vector<std::int16_t>;
 
-    int left_;
-    int right_;
-    int top_;
-    int bottom_;
 
-    Image image_;
+	int left_;
+	int right_;
+	int top_;
+	int bottom_;
+
+	Image image_;
 }; // Sprite
 
 
