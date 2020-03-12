765094c1d979f0dcd28f4c1e0e09bf6edc4fe301
Boris I. Bendovsky <bibendovsky@hotmail.com>
2020-03-12 11:20:00 +0200

    Remove field old_rpower

--- src/3d_def.h
+++ src/3d_def.h
@@ -2503,9 +2503,6 @@ struct gametype
 	std::int16_t rpower;
 
 	// !!! Used in saved game.
-	std::int16_t old_rpower;
-
-	// !!! Used in saved game.
 	std::int8_t rzoom;
 
 	// !!! Used in saved game.
