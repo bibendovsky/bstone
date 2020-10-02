42027a4307389b9953f752b2a4f7cc290c7cb400
Boris I. Bendovsky <bibendovsky@hotmail.com>
2020-10-02 18:47:35 +0300

    Simplify fixed_to_floating

--- src/bstone_math.h
+++ src/bstone_math.h
@@ -62,13 +62,13 @@ constexpr double rad_to_deg(
 constexpr double fixed_to_floating(
 	const int value)
 {
-	return (value >> 16) + ((value & 0xFFFF) / 65'536.0);
+	return static_cast<double>(value) / static_cast<double>(1 << 16);
 }
 
 constexpr int floating_to_fixed(
 	const double value)
 {
-	return static_cast<int>(value * 65'536.0);
+	return static_cast<int>(value * (1 << 16));
 }
 
 
