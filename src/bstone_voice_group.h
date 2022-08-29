53a649d0149414daa871c22ee6a82f3c34f22a8f
Boris I. Bendovsky <bibendovsky@hotmail.com>
2022-08-29 22:07:04 +0300

    [VOICE_GROUP] Add method to set voice's gain

--- src/bstone_voice_group.h
+++ src/bstone_voice_group.h
@@ -13,17 +13,37 @@ public:
 	VoiceGroup() noexcept = default;
 	virtual ~VoiceGroup() = default;
 
-	virtual VoiceGroup& add_group() = 0;
-	virtual void remove_group(VoiceGroup& group) = 0;
+	VoiceGroup& add_group();
+	void remove_group(VoiceGroup& group);
 
-	virtual bool is_any_playing() = 0;
-	virtual void set_gain(double gain) = 0;
-	virtual void pause() = 0;
-	virtual void resume() = 0;
-	virtual void stop() = 0;
+	bool is_any_playing();
+	void set_gain(double gain);
+	void pause();
+	void resume();
+	void stop();
 
-	virtual void add_voice(AudioMixerVoiceHandle handle) = 0;
-	virtual void remove_voice(AudioMixerVoiceHandle handle) = 0;
+	void add_voice(AudioMixerVoiceHandle handle);
+	void remove_voice(AudioMixerVoiceHandle handle);
+
+	void set_voice_gain(AudioMixerVoiceHandle handle, double gain);
+
+protected:
+	[[noreturn]] static void fail_nested(const char* message);
+
+private:
+	virtual VoiceGroup& do_add_group() = 0;
+	virtual void do_remove_group(VoiceGroup& group) = 0;
+
+	virtual bool do_is_any_playing() = 0;
+	virtual void do_set_gain(double gain) = 0;
+	virtual void do_pause() = 0;
+	virtual void do_resume() = 0;
+	virtual void do_stop() = 0;
+
+	virtual void do_add_voice(AudioMixerVoiceHandle handle) = 0;
+	virtual void do_remove_voice(AudioMixerVoiceHandle handle) = 0;
+
+	virtual void do_set_voice_gain(AudioMixerVoiceHandle handle, double gain) = 0;
 }; // VoiceGroup
 
 // ==========================================================================
