ba586730ce5461c63aa75e996335d25ceead64ef
Boris I. Bendovsky <bibendovsky@hotmail.com>
2022-09-06 14:22:52 +0300

    [AUDIO_MIXER] Revise voice group

--- src/bstone_voice_group.h
+++ src/bstone_voice_group.h
@@ -1,11 +1,11 @@
-#ifndef BSTONE_VOICE_GROUP_MGR_INCLUDED
-#define BSTONE_VOICE_GROUP_MGR_INCLUDED
+#ifndef BSTONE_VOICE_GROUP_INCLUDED
+#define BSTONE_VOICE_GROUP_INCLUDED
 
 #include "bstone_audio_mixer.h"
+#include "bstone_voice.h"
 #include <memory>
 
-namespace bstone
-{
+namespace bstone {
 
 class VoiceGroup
 {
@@ -13,38 +13,32 @@ public:
 	VoiceGroup() noexcept = default;
 	virtual ~VoiceGroup() = default;
 
-	VoiceGroup& add_group();
-	void remove_group(VoiceGroup& group);
-
 	bool is_any_playing();
 	void set_gain(double gain);
 	void pause();
 	void resume();
 	void stop();
 
-	void add_voice(AudioMixerVoiceHandle handle);
-	void remove_voice(AudioMixerVoiceHandle handle);
-
-	void set_voice_gain(AudioMixerVoiceHandle handle, double gain);
+	void add_voice(Voice& voice);
+	void stop_and_remove_voice(Voice& voice);
+	void stop_voice(Voice& voice);
+	void set_voice_gain(Voice& voice, double gain);
 
 protected:
 	[[noreturn]] static void fail_nested(const char* message);
 
 private:
-	virtual VoiceGroup& do_add_group() = 0;
-	virtual void do_remove_group(VoiceGroup& group) = 0;
-
 	virtual bool do_is_any_playing() = 0;
 	virtual void do_set_gain(double gain) = 0;
 	virtual void do_pause() = 0;
 	virtual void do_resume() = 0;
 	virtual void do_stop() = 0;
 
-	virtual void do_add_voice(AudioMixerVoiceHandle handle) = 0;
-	virtual void do_remove_voice(AudioMixerVoiceHandle handle) = 0;
-
-	virtual void do_set_voice_gain(AudioMixerVoiceHandle handle, double gain) = 0;
-}; // VoiceGroup
+	virtual void do_add_voice(Voice& voice) = 0;
+	virtual void do_stop_and_remove_voice(Voice& voice) = 0;
+	virtual void do_stop_voice(Voice& voice) = 0;
+	virtual void do_set_voice_gain(Voice& voice, double gain) = 0;
+};
 
 // ==========================================================================
 
@@ -54,4 +48,4 @@ VoiceGroupUPtr make_voice_group(AudioMixer& audio_mixer);
 
 } // bstone
 
-#endif // !BSTONE_VOICE_GROUP_MGR_INCLUDED
+#endif // !BSTONE_VOICE_GROUP_INCLUDED
