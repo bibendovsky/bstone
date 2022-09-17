e31e25e76be0cd697768d270931bf5a2a9ce1f12
Boris I. Bendovsky <bibendovsky@hotmail.com>
2022-09-17 23:53:21 +0300

    Use SPDX license identifier Update copyright years

--- src/bstone_memory_resource.h
+++ src/bstone_memory_resource.h
@@ -1,3 +1,10 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 1992-2013 Apogee Entertainment, LLC
+Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com)
+SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
 #ifndef BSTONE_MEMORY_RESOURCE_INCLUDED
 #define BSTONE_MEMORY_RESOURCE_INCLUDED
 
--- src/bstone_voice_group.h
+++ src/bstone_voice_group.h
@@ -1,3 +1,10 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 1992-2013 Apogee Entertainment, LLC
+Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com)
+SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
 #ifndef BSTONE_VOICE_GROUP_INCLUDED
 #define BSTONE_VOICE_GROUP_INCLUDED
 
