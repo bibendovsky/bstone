3dea1ef72a101519afd17aa95c881da40a18040d
Boris I. Bendovsky <bibendovsky@hotmail.com>
2022-09-18 20:14:48 +0300

    Relicense completely new source code under MIT

--- src/bstone_math.h
+++ src/bstone_math.h
@@ -1,8 +1,7 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 1992-2013 Apogee Entertainment, LLC
-Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com)
-SPDX-License-Identifier: GPL-2.0-or-later
+Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+SPDX-License-Identifier: MIT
 */
 
 //
