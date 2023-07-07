8046e17595be4cc9b94282db6304ab97fa1a7583
Boris I. Bendovsky <bibendovsky@hotmail.com>
2023-07-07 18:11:21 +0300

    Remove bstone_c_str

--- src/bstone_c_str.cpp
+++
@@ -1,9 +0,0 @@
-/*
-BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
-SPDX-License-Identifier: MIT
-*/
-
-// C-string operations.
-
-#include "bstone_c_str.h"
