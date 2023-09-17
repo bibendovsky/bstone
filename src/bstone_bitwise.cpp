1a109ff1aca2d9ca0d617acd0a28b15b32c8f7fa
Boris I. Bendovsky <bibendovsky@hotmail.com>
2023-09-17 18:11:04 +0300

    Remove bstone_bitwise

--- src/bstone_bitwise.cpp
+++
@@ -1,7 +0,0 @@
-/*
-BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
-SPDX-License-Identifier: MIT
-*/
-
-#include "bstone_bitwise.h"
