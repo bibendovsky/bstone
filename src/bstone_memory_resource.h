2005be9054690fde773446bd827a6f913f5498f6
Boris I. Bendovsky <bibendovsky@hotmail.com>
2023-09-22 18:17:35 +0300

    Add unque_ptr for memory resource

--- src/bstone_memory_resource.h
+++ src/bstone_memory_resource.h
@@ -7,7 +7,10 @@ SPDX-License-Identifier: MIT
 #if !defined(BSTONE_MEMORY_RESOURCE_INCLUDED)
 #define BSTONE_MEMORY_RESOURCE_INCLUDED
 
+#include <memory>
+
 #include "bstone_int.h"
+#include "bstone_memory.h"
 
 namespace bstone {
 
@@ -24,6 +27,58 @@ private:
 	virtual void* do_allocate(IntP size) = 0;
 	virtual void do_deallocate(void* ptr) = 0;
 };
+
+// ==========================================================================
+
+template<typename T>
+class MemoryResourceUPtrDeleter
+{
+public:
+	MemoryResourceUPtrDeleter(MemoryResource& memory_resource);
+
+	void operator()(T* ptr) const;
+
+private:
+	MemoryResource* memory_resource_{};
+};
+
+// --------------------------------------------------------------------------
+
+template<typename T>
+MemoryResourceUPtrDeleter<T>::MemoryResourceUPtrDeleter(MemoryResource& memory_resource)
+	:
+	memory_resource_{&memory_resource}
+{}
+
+template<typename T>
+void MemoryResourceUPtrDeleter<T>::operator()(T* ptr) const
+{
+	bstone::destroy_at(ptr);
+	memory_resource_->deallocate(ptr);
+}
+
+// ==========================================================================
+
+template<typename T>
+using MemoryResourceUPtr = std::unique_ptr<T, MemoryResourceUPtrDeleter<T>>;
+
+template<typename T, typename ...TArgs>
+auto make_memory_resource_uptr(MemoryResource& memory_resource, TArgs&& ...args)
+{
+	const auto instance = static_cast<T*>(memory_resource.allocate(static_cast<IntP>(sizeof(T))));
+
+	try
+	{
+		bstone::construct_at(instance, std::forward<TArgs>(args)...);
+	}
+	catch (...)
+	{
+		memory_resource.deallocate(instance);
+		throw;
+	}
+
+	return MemoryResourceUPtr<T>{instance, MemoryResourceUPtrDeleter<T>{memory_resource}};
+}
 
 // ==========================================================================
 
--- src/tests/src/bstone_tests_memory_resource.cpp
+++ src/tests/src/bstone_tests_memory_resource.cpp
@@ -8,13 +8,95 @@ auto tester = bstone::Tester{};
 
 // ==========================================================================
 
+// make_memory_resource_uptr(MemoryResource&, TArgs&& ...)
+void test_glnf5xdb4bsrfzxd()
+{
+	class Value
+	{
+	public:
+		Value(int value, bool& is_destructed)
+			:
+			value_{value},
+			is_destructed_{is_destructed}
+		{}
+
+		~Value()
+		{
+			is_destructed_ = true;
+		}
+
+		int get_value() const noexcept
+		{
+			return value_;
+		}
+
+	private:
+		int value_{};
+		bool& is_destructed_;
+	};
+
+	constexpr auto ref_value = 100;
+
+	auto is_failed = false;
+	auto is_destructed = false;
+	auto value = 0;
+
+	try
+	{
+		const auto value_uptr = bstone::make_memory_resource_uptr<Value>(
+			bstone::get_new_delete_memory_resource(),
+			ref_value,
+			is_destructed);
+
+		value = (*value_uptr).get_value();
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(!is_failed && is_destructed && value == ref_value);
+}
+
+// ==========================================================================
+
+// NullMemoryResource and get_null_memory_resource()
+void test_yzz2zm9bqm3j4xvs()
+{
+	auto& memory_resource = bstone::get_null_memory_resource();
+
+	void* ptr = nullptr;
+	auto is_allocated = false;
+
+	try
+	{
+		ptr = memory_resource.allocate(0);
+		is_allocated = true;
+	}
+	catch (...) {}
+
+	auto is_deallocated = false;
+
+	try
+	{
+		memory_resource.deallocate(ptr);
+		is_deallocated = true;
+	}
+	catch (...) {}
+
+	tester.check(!is_allocated && is_deallocated);
+}
+
+// ==========================================================================
+
+// NewDeleteMemoryResource and get_new_delete_memory_resource()
 void test_g6rkthvhbwtdcko3()
 {
 	auto is_succeeded = false;
 
 	try
 	{
-		auto memory_resource = bstone::NewDeleteMemoryResource{};
+		auto& memory_resource = bstone::get_new_delete_memory_resource();
 		const auto ptr = memory_resource.allocate(1);
 		memory_resource.deallocate(ptr);
 		is_succeeded = true;
@@ -24,6 +106,7 @@ void test_g6rkthvhbwtdcko3()
 	tester.check(is_succeeded);
 }
 
+// get_default_memory_resource()
 void test_yvspu8cqcffe2589()
 {
 	auto is_succeeded = false;
@@ -47,12 +130,24 @@ class Registrator
 public:
 	Registrator()
 	{
-		register_new_delete();
+		register_make_unique_ptr();
+		register_null_memory_resource();
+		register_new_delete_memory_resource();
 		register_get_default_memory_resource();
 	}
 
 private:
-	void register_new_delete()
+	void register_make_unique_ptr()
+	{
+		tester.register_test("make_memory_resource_uptr#glnf5xdb4bsrfzxd", test_glnf5xdb4bsrfzxd);
+	}
+
+	void register_null_memory_resource()
+	{
+		tester.register_test("NullMemoryResource#yzz2zm9bqm3j4xvs", test_yzz2zm9bqm3j4xvs);
+	}
+
+	void register_new_delete_memory_resource()
 	{
 		tester.register_test("NewDeleteMemoryResource#g6rkthvhbwtdcko3", test_g6rkthvhbwtdcko3);
 	}
