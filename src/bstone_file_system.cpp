afbec460b8fb30224b558b676e8d73e90f058700
Boris I. Bendovsky <bibendovsky@hotmail.com>
2023-12-03 20:38:48 +0200

    Revise bstone_file_system

--- src/bstone_file_system.cpp
+++ src/bstone_file_system.cpp
@@ -4,88 +4,43 @@ Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
+// File system utils.
 
-#include "bstone_file_system.h"
-
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif // !NOMINMAX
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif // !WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#else
-#include <sys/stat.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#endif // _WIN32
-
-#include "bstone_encoding.h"
 #include "bstone_exception.h"
+#include "bstone_file_system.h"
+#include "bstone_fs.h"
 
+namespace bstone {
+namespace file_system {
 
-namespace bstone
-{
-namespace file_system
-{
-
-
-namespace detail
-{
-
-
-char get_separator() noexcept
-{
-	return
-#if _WIN32
-		'\\'
-#else
-		'/'
-#endif // _WIN32
-		;
-}
-
-
-} // detail
-
-
-std::string normalize_path(
-	const std::string& path)
+std::string normalize_path(const std::string& path)
 {
 	auto result = path;
 
 	for (auto& ch : result)
 	{
-		if ((ch == '/' || ch == '\\') && ch != detail::get_separator())
+		if ((ch == '/' || ch == '\\') && ch != fs::native_separator)
 		{
-			ch = detail::get_separator();
+			ch = fs::native_separator;
 		}
 	}
 
 	return result;
 }
 
-std::string append_path_separator(
-	const std::string& path)
+std::string append_path_separator(const std::string& path)
 {
 	auto result = path;
 
-	if (!result.empty() && result.back() != detail::get_separator())
+	if (!result.empty() && result.back() != fs::native_separator)
 	{
-		result += detail::get_separator();
+		result += fs::native_separator;
 	}
 
 	return result;
 }
 
-std::string append_path(
-	const std::string& path,
-	const std::string& sub_path)
+std::string append_path(const std::string& path, const std::string& sub_path)
 {
 	if (path.empty() && sub_path.empty())
 	{
@@ -103,24 +58,21 @@ std::string append_path(
 	else
 	{
 		auto result = std::string{};
-		result.reserve(path.size() + 1 + sub_path.size());
+		result.reserve(path.size() + sub_path.size() + 1);
 		result.assign(path);
 
-		if (result.back() != detail::get_separator())
+		if (result.back() != fs::native_separator)
 		{
-			result += detail::get_separator();
+			result += fs::native_separator;
 		}
 
 		result += sub_path;
-
 		return result;
 	}
 }
 
-void replace_extension(
-	std::string& path_name,
-	const std::string& new_extension)
-try {
+void replace_extension(std::string& path_name, const std::string& new_extension)
+{
 	if (path_name.empty() || new_extension.empty())
 	{
 		return;
@@ -145,228 +97,22 @@ try {
 	}
 
 	path_name += new_extension;
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-std::string get_working_dir()
-try {
-#if _WIN32
-	const auto utf16_string_size = GetCurrentDirectoryW(0, nullptr);
-
-	if (utf16_string_size == 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("GetCurrentDirectoryW");
-	}
-
-	auto utf16_dir = std::u16string{};
-	utf16_dir.resize(utf16_string_size - 1);
-
-	const auto win32_result = GetCurrentDirectoryW(
-		utf16_string_size,
-		reinterpret_cast<LPWSTR>(&utf16_dir[0])
-	);
-
-	if (win32_result == 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("GetCurrentDirectoryW");
-	}
-
-	utf16_dir.resize(win32_result);
-
-	return u16_to_u8(utf16_dir);
-#else
-	const auto max_size = pathconf(".", _PC_PATH_MAX);
-
-	if (max_size <= 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("Unknown max path length.");
-	}
-
-	constexpr auto initial_size = 256;
-	constexpr auto size_delta = 256;
-
-	auto size = initial_size;
-	auto result = std::string{};
-
-	while (true)
-	{
-		result.resize(size - 1);
-
-		const auto posix_result = getcwd(&result[0], size);
-
-		if (posix_result != nullptr)
-		{
-			break;
-		}
-
-		switch (errno)
-		{
-			case ERANGE:
-				break;
-
-			case EACCES:
-				BSTONE_THROW_STATIC_SOURCE("No access.");
-
-			default:
-				BSTONE_THROW_STATIC_SOURCE("Generic error.");
-		}
-
-		size += size_delta;
-
-		if (size > max_size)
-		{
-			BSTONE_THROW_STATIC_SOURCE("Path too long.");
-		}
-	}
-
-	result.resize(std::string::traits_type::length(result.c_str()));
-
-	return result;
-#endif // _WIN32
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-std::string resolve_path(
-	const std::string& path)
-try {
-	if (path.empty())
-	{
-		BSTONE_THROW_STATIC_SOURCE("Empty path.");
-	}
-
-#ifdef _WIN32
-	const auto& utf16_path = u8_to_u16(path);
-
-	const auto utf16_full_path_size = GetFullPathNameW(
-		reinterpret_cast<LPCWSTR>(utf16_path.c_str()),
-		0,
-		nullptr,
-		nullptr
-	);
-
-	if (utf16_full_path_size == 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("GetFullPathNameW");
-	}
-
-	auto utf16_full_path = std::u16string{};
-	utf16_full_path.resize(utf16_full_path_size - 1);
-
-	const auto win32_result = GetFullPathNameW(
-		reinterpret_cast<LPCWSTR>(utf16_path.c_str()),
-		utf16_full_path_size,
-		reinterpret_cast<LPWSTR>(&utf16_full_path[0]),
-		nullptr
-	);
-
-	if (win32_result == 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("GetFullPathNameW");
-	}
-
-	utf16_full_path.resize(win32_result);
-
-	return u16_to_u8(utf16_full_path);
-#else
-	const auto max_size = pathconf(".", _PC_PATH_MAX);
-
-	if (max_size <= 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("Unknown max path length.");
-	}
-
-	auto result = std::string{};
-	result.resize(max_size - 1);
-
-	const auto posix_result = realpath(path.c_str(), &result[0]);
-
-	if (posix_result == nullptr)
-	{
-		BSTONE_THROW_STATIC_SOURCE("realpath");
-	}
-
-	result.resize(std::string::traits_type::length(result.c_str()));
-
-	return result;
-#endif // _WIN32
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-bool has_file(
-	const std::string& path)
-{
-	if (path.empty())
-	{
-		return false;
-	}
-
-#if _WIN32
-	const auto& utf16_path = u8_to_u16(path);
-
-	WIN32_FIND_DATAW win32_fd;
-
-	const auto win32_result = FindFirstFileW(
-		reinterpret_cast<LPCWSTR>(utf16_path.c_str()),
-		&win32_fd
-	);
-
-	if (win32_result == INVALID_HANDLE_VALUE)
-	{
-		return false;
-	}
-
-	static_cast<void>(FindClose(win32_result));
-
-	return true;
-#else
-	struct stat posix_stat;
-
-	const auto posix_result = stat(path.c_str(), &posix_stat);
-
-	if (posix_result != 0)
-	{
-		return false;
-	}
-
-	return S_ISREG(posix_stat.st_mode) != 0;
-#endif // _WIN32
 }
 
-void rename(
-	const std::string& old_path,
-	const std::string& new_path)
-try {
-	if (old_path.empty())
-	{
-		BSTONE_THROW_STATIC_SOURCE("Empty old path.");
-	}
+std::string get_working_dir()
+{
+	constexpr auto max_path_size = 4096;
+	auto working_directory = std::string{};
+	working_directory.resize(max_path_size);
+	const auto path_size = fs::get_working_directory(&working_directory.front(), max_path_size);
+	working_directory.resize(path_size);
+	return working_directory;
+}
 
-	if (new_path.empty())
-	{
-		BSTONE_THROW_STATIC_SOURCE("Empty new path.");
-	}
-
-#if _WIN32
-	const auto& old_path_utf16 = u8_to_u16(old_path);
-	const auto& new_path_utf16 = u8_to_u16(new_path);
-
-	const auto win32_move_file_result = MoveFileExW(
-		reinterpret_cast<LPCWSTR>(old_path_utf16.c_str()),
-		reinterpret_cast<LPCWSTR>(new_path_utf16.c_str()),
-		MOVEFILE_REPLACE_EXISTING
-	);
-
-	if (win32_move_file_result == FALSE)
-	{
-		BSTONE_THROW_STATIC_SOURCE("MoveFileExW");
-	}
-#else
-	const auto posix_rename_result = ::rename(old_path.c_str(), new_path.c_str());
-
-	if (posix_rename_result != 0)
-	{
-		BSTONE_THROW_STATIC_SOURCE("rename");
-	}
-#endif // _WIN32
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
+void rename(const std::string& old_path, const std::string& new_path)
+{
+	fs::rename(old_path.c_str(), new_path.c_str());
+}
 
 } // file_system
 } // bstone
--- src/bstone_file_system.h
+++ src/bstone_file_system.h
@@ -4,54 +4,24 @@ Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-
-//
 // File system utils.
-//
 
-
-#ifndef BSTONE_FILE_SYSTEM_INCLUDED
+#if !defined(BSTONE_FILE_SYSTEM_INCLUDED)
 #define BSTONE_FILE_SYSTEM_INCLUDED
-
 
 #include <string>
 
+namespace bstone {
+namespace file_system {
 
-namespace bstone
-{
-namespace file_system
-{
-
-
-std::string normalize_path(
-	const std::string& path);
-
-std::string append_path_separator(
-	const std::string& path);
-
-std::string append_path(
-	const std::string& path,
-	const std::string& sub_path);
-
-void replace_extension(
-	std::string& path_name,
-	const std::string& new_extension);
-
+std::string normalize_path(const std::string& path);
+std::string append_path_separator(const std::string& path);
+std::string append_path(const std::string& path, const std::string& sub_path);
+void replace_extension(std::string& path_name, const std::string& new_extension);
 std::string get_working_dir();
-
-std::string resolve_path(
-	const std::string& path);
-
-bool has_file(
-	const std::string& path);
-
-void rename(
-	const std::string& old_path,
-	const std::string& new_path);
-
+void rename(const std::string& old_path, const std::string& new_path);
 
 } // file_system
 } // bstone
 
-
-#endif // !BSTONE_FILE_SYSTEM_INCLUDED
+#endif // BSTONE_FILE_SYSTEM_INCLUDED
