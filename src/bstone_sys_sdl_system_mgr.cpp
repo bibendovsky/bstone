f16f476891f60540fbfc086346975ec18205aa13
Boris I. Bendovsky <bibendovsky@hotmail.com>
2023-12-10 16:40:07 +0200

    [SDL] Enable only required event types

--- src/bstone_sys_sdl_system_mgr.cpp
+++ src/bstone_sys_sdl_system_mgr.cpp
@@ -18,6 +18,52 @@ SPDX-License-Identifier: MIT
 #include "bstone_sys_sdl_video_mgr.h"
 #include "bstone_sys_system_mgr.h"
 
+#define BSTONE_SDL_2_0_2 SDL_VERSION_ATLEAST(2,0,2)
+#define BSTONE_SDL_2_0_4 SDL_VERSION_ATLEAST(2,0,4)
+#define BSTONE_SDL_2_0_5 SDL_VERSION_ATLEAST(2,0,5)
+#define BSTONE_SDL_2_0_9 SDL_VERSION_ATLEAST(2,0,9)
+#define BSTONE_SDL_2_0_14 SDL_VERSION_ATLEAST(2,0,14)
+#define BSTONE_SDL_2_0_22 SDL_VERSION_ATLEAST(2,0,22)
+#define BSTONE_SDL_2_24_0 SDL_VERSION_ATLEAST(2,24,0)
+
+#if !BSTONE_SDL_2_0_2
+#define SDL_RENDER_TARGETS_RESET (0x2000)
+#endif
+
+#if !BSTONE_SDL_2_0_4
+#define SDL_KEYMAPCHANGED (0x304)
+#define SDL_AUDIODEVICEADDED (0x1100)
+#define SDL_AUDIODEVICEREMOVED (0x1101)
+#define SDL_RENDER_DEVICE_RESET (0x2001)
+#endif
+
+#if !BSTONE_SDL_2_0_5
+#define SDL_DROPTEXT (0x1001)
+#define SDL_DROPBEGIN (0x1002)
+#define SDL_DROPCOMPLETE (0x1003)
+#endif
+
+#if !BSTONE_SDL_2_0_9
+#define SDL_DISPLAYEVENT (0x150)
+#define SDL_SENSORUPDATE (0x1200)
+#endif
+
+#if !BSTONE_SDL_2_0_14
+#define SDL_LOCALECHANGED (0x107)
+#define SDL_CONTROLLERTOUCHPADDOWN (0x656)
+#define SDL_CONTROLLERTOUCHPADMOTION (0x657)
+#define SDL_CONTROLLERTOUCHPADUP (0x658)
+#define SDL_CONTROLLERSENSORUPDATE (0x659)
+#endif
+
+#if !BSTONE_SDL_2_0_22
+#define SDL_TEXTEDITING_EXT (0x305)
+#endif
+
+#if !BSTONE_SDL_2_24_0
+#define SDL_JOYBATTERYUPDATED (0x607)
+#endif
+
 namespace bstone {
 namespace sys {
 
@@ -45,6 +91,8 @@ private:
 private:
 	static MemoryResource& get_memory_resource();
 
+	static void configure_event_types() noexcept;
+
 	void log_version(const SDL_version& sdl_version, StringView version_name);
 	void log_compiled_version();
 	void log_linked_version();
@@ -62,6 +110,7 @@ try
 	logger_.log_information("<<< Start up SDL system manager.");
 
 	log_versions();
+	configure_event_types();
 	sdl_ensure_result(SDL_Init(0));
 
 	logger_.log_information(">>> SDL system manager started up.");
@@ -104,6 +153,79 @@ MemoryResource& SdlSystemMgr::get_memory_resource()
 	static SinglePoolMemoryResource<SdlSystemMgr> memory_pool{};
 
 	return memory_pool;
+}
+
+void SdlSystemMgr::configure_event_types() noexcept
+{
+	SDL_EventState(SDL_QUIT, SDL_ENABLE);
+	SDL_EventState(SDL_APP_TERMINATING, SDL_DISABLE);
+	SDL_EventState(SDL_APP_LOWMEMORY, SDL_DISABLE);
+	SDL_EventState(SDL_APP_WILLENTERBACKGROUND, SDL_DISABLE);
+	SDL_EventState(SDL_APP_DIDENTERBACKGROUND, SDL_DISABLE);
+	SDL_EventState(SDL_APP_WILLENTERFOREGROUND, SDL_DISABLE);
+	SDL_EventState(SDL_APP_DIDENTERFOREGROUND, SDL_DISABLE);
+
+	SDL_EventState(SDL_LOCALECHANGED, SDL_DISABLE); // v2.0.14+ (0x107)
+
+	SDL_EventState(SDL_DISPLAYEVENT, SDL_DISABLE); // v2.0.9+ (0x150)
+
+	SDL_EventState(SDL_WINDOWEVENT, SDL_ENABLE);
+	SDL_EventState(SDL_SYSWMEVENT, SDL_DISABLE);
+
+	SDL_EventState(SDL_KEYDOWN, SDL_ENABLE);
+	SDL_EventState(SDL_KEYUP, SDL_ENABLE);
+	SDL_EventState(SDL_TEXTEDITING, SDL_DISABLE);
+	SDL_EventState(SDL_TEXTINPUT, SDL_DISABLE);
+	SDL_EventState(SDL_KEYMAPCHANGED, SDL_DISABLE); // v2.0.4+ (0x304)
+	SDL_EventState(SDL_TEXTEDITING_EXT, SDL_DISABLE); // v2.0.22+ (0x305)
+
+	SDL_EventState(SDL_MOUSEMOTION, SDL_ENABLE);
+	SDL_EventState(SDL_MOUSEBUTTONDOWN, SDL_ENABLE);
+	SDL_EventState(SDL_MOUSEBUTTONUP, SDL_ENABLE);
+	SDL_EventState(SDL_MOUSEWHEEL, SDL_ENABLE);
+
+	SDL_EventState(SDL_JOYAXISMOTION, SDL_DISABLE);
+	SDL_EventState(SDL_JOYBALLMOTION, SDL_DISABLE);
+	SDL_EventState(SDL_JOYHATMOTION, SDL_DISABLE);
+	SDL_EventState(SDL_JOYBUTTONDOWN, SDL_DISABLE);
+	SDL_EventState(SDL_JOYBUTTONUP, SDL_DISABLE);
+	SDL_EventState(SDL_JOYDEVICEADDED, SDL_DISABLE);
+	SDL_EventState(SDL_JOYDEVICEREMOVED, SDL_DISABLE);
+	SDL_EventState(SDL_JOYBATTERYUPDATED, SDL_DISABLE); // v2.24.0+ (0x607)
+
+	SDL_EventState(SDL_CONTROLLERAXISMOTION, SDL_DISABLE);
+	SDL_EventState(SDL_CONTROLLERBUTTONDOWN, SDL_DISABLE);
+	SDL_EventState(SDL_CONTROLLERBUTTONUP, SDL_DISABLE);
+	SDL_EventState(SDL_CONTROLLERDEVICEADDED, SDL_DISABLE);
+	SDL_EventState(SDL_CONTROLLERDEVICEREMOVED, SDL_DISABLE);
+	SDL_EventState(SDL_CONTROLLERDEVICEREMAPPED, SDL_DISABLE);
+	SDL_EventState(SDL_CONTROLLERTOUCHPADDOWN, SDL_DISABLE); // v2.0.14+ (0x656)
+	SDL_EventState(SDL_CONTROLLERTOUCHPADMOTION, SDL_DISABLE); // v2.0.14+ (0x657)
+	SDL_EventState(SDL_CONTROLLERTOUCHPADUP, SDL_DISABLE); // v2.0.14+ (0x658)
+	SDL_EventState(SDL_CONTROLLERSENSORUPDATE, SDL_DISABLE); // v2.0.14+ (0x659)
+
+	SDL_EventState(SDL_FINGERDOWN, SDL_DISABLE);
+	SDL_EventState(SDL_FINGERUP, SDL_DISABLE);
+	SDL_EventState(SDL_FINGERMOTION, SDL_DISABLE);
+
+	SDL_EventState(SDL_DOLLARGESTURE, SDL_DISABLE);
+	SDL_EventState(SDL_DOLLARRECORD, SDL_DISABLE);
+	SDL_EventState(SDL_MULTIGESTURE, SDL_DISABLE);
+
+	SDL_EventState(SDL_CLIPBOARDUPDATE, SDL_DISABLE);
+
+	SDL_EventState(SDL_DROPFILE, SDL_DISABLE);
+	SDL_EventState(SDL_DROPTEXT, SDL_DISABLE); // v2.0.5+ (0x1001)
+	SDL_EventState(SDL_DROPBEGIN, SDL_DISABLE); // v2.0.5+ (0x1002)
+	SDL_EventState(SDL_DROPCOMPLETE, SDL_DISABLE); // v2.0.5+ (0x1003)
+
+	SDL_EventState(SDL_AUDIODEVICEADDED, SDL_ENABLE); // v2.0.4+ (0x1100)
+	SDL_EventState(SDL_AUDIODEVICEREMOVED, SDL_ENABLE); // v2.0.4+ (0x1101)
+
+	SDL_EventState(SDL_SENSORUPDATE, SDL_DISABLE); // v2.0.9+ (0x1200)
+
+	SDL_EventState(SDL_RENDER_TARGETS_RESET, SDL_DISABLE); // v2.0.2+ (0x2000)
+	SDL_EventState(SDL_RENDER_DEVICE_RESET, SDL_DISABLE); // v2.0.4+ (0x2001)
 }
 
 void SdlSystemMgr::log_version(const SDL_version& sdl_version, StringView version_name)
