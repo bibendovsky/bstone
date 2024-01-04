07cde2abd234a783968f59f59ed23c1740cecf98
Boris I. Bendovsky <bibendovsky@hotmail.com>
2024-01-04 18:57:53 +0200

    Revise bstone_sys_color

---
+++ src/bstone_sys_color.cpp
@@ -0,0 +1,9 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+SPDX-License-Identifier: MIT
+*/
+
+// RGBA color, 8-bit per component.
+
+#include "bstone_sys_color.h"
