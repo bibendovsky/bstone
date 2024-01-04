9cc2643b98733e8c1285f3532fd31261d3286a23
Boris I. Bendovsky <bibendovsky@hotmail.com>
2024-01-04 19:21:24 +0200

    Revise bstone_sys_system_mgr

--- src/bstone_sys_system_mgr.cpp
+++ src/bstone_sys_system_mgr.cpp
@@ -1,29 +1,30 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
-#include "bstone_exception.h"
+// System manager.
+
 #include "bstone_sys_system_mgr.h"
 
 namespace bstone {
 namespace sys {
 
 AudioMgr& SystemMgr::get_audio_mgr()
-try {
+{
 	return do_get_audio_mgr();
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+}
 
 EventMgr& SystemMgr::get_event_mgr()
-try {
+{
 	return do_get_event_mgr();
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+}
 
 VideoMgr& SystemMgr::get_video_mgr()
-try {
+{
 	return do_get_video_mgr();
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+}
 
 } // namespace sys
 } // namespace bstone
--- src/bstone_sys_system_mgr.h
+++ src/bstone_sys_system_mgr.h
@@ -1,13 +1,16 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2022 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
+
+// System manager.
 
 #if !defined(BSTONE_SYS_SYSTEM_MGR_INCLUDED)
 #define BSTONE_SYS_SYSTEM_MGR_INCLUDED
 
 #include <memory>
+
 #include "bstone_sys_audio_mgr.h"
 #include "bstone_sys_event_mgr.h"
 #include "bstone_sys_logger.h"
