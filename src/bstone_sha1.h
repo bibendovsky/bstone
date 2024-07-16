de0511e6f1cf28c179e801b25d1bd5d506ed3927
Boris I. Bendovsky <bibendovsky@hotmail.com>
2024-07-16 11:52:08 +0300

    [SHA1] Add functions to make a digest

--- src/bstone_sha1.h
+++ src/bstone_sha1.h
@@ -1,18 +1,21 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2023 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
 // SHA-1 implementation based on RFC 3174 sample code.
 // https://www.ietf.org/rfc/rfc3174.txt
 
-#if !defined(BSTONE_SHA1_INCLUDED)
+#ifndef BSTONE_SHA1_INCLUDED
 #define BSTONE_SHA1_INCLUDED
 
 #include <cstdint>
 
 #include "bstone_array.h"
+#include "bstone_char_conv.h"
+#include "bstone_char_traits.h"
+#include "bstone_exception.h"
 #include "bstone_span.h"
 
 namespace bstone {
@@ -70,6 +73,27 @@ template<typename T>
 void Sha1::process(Span<T> span)
 {
 	process(span.get_data(), span.get_bytes_size());
+}
+
+// ==========================================================================
+
+template<typename TChar>
+constexpr Sha1Digest make_sha1_digest(const TChar* chars, std::intptr_t char_count)
+{
+	if (char_count != sha1_digest_char_count)
+	{
+		BSTONE_THROW_STATIC_SOURCE("Invalid SHA1 string length.");
+	}
+
+	auto result = Sha1Digest{};
+	hex_chars_to_bytes(chars, chars + char_count, result.begin(), result.end());
+	return result;
+}
+
+template<typename TChar>
+constexpr Sha1Digest make_sha1_digest(const TChar* chars)
+{
+	return make_sha1_digest(chars, char_traits::get_size(chars));
 }
 
 } // namespace bstone
--- src/tests/src/tests/src/bstone_tests_sha1.cpp
+++ src/tests/src/tests/src/bstone_tests_sha1.cpp
@@ -152,12 +152,210 @@ void test_hka86bzux5vmorkw()
 
 // ==========================================================================
 
+// constexpr Sha1Digest make_sha1_digest(const TChar*, std::intptr_t)
+// char
+void test_0km1jccde7pdny02()
+{
+	constexpr auto ref_digest = bstone::Sha1Digest
+	{
+		std::uint8_t{0xB6}, std::uint8_t{0x58}, std::uint8_t{0x9F}, std::uint8_t{0xC6}, std::uint8_t{0xAB},
+		std::uint8_t{0x0D}, std::uint8_t{0xC8}, std::uint8_t{0x2C}, std::uint8_t{0xF1}, std::uint8_t{0x20},
+		std::uint8_t{0x99}, std::uint8_t{0xD1}, std::uint8_t{0xC2}, std::uint8_t{0xD4}, std::uint8_t{0x0A},
+		std::uint8_t{0xB9}, std::uint8_t{0x94}, std::uint8_t{0xE8}, std::uint8_t{0x41}, std::uint8_t{0x0C},
+	};
+
+	constexpr auto digest = bstone::make_sha1_digest(
+		"b6589fc6ab0dc82cf12099d1c2d40aB994E8410C???", bstone::sha1_digest_char_count);
+
+	constexpr auto is_valid = digest == ref_digest;
+
+	tester.check(is_valid);
+}
+
+// constexpr Sha1Digest make_sha1_digest(const TChar*, std::intptr_t)
+// char
+// Invalid string length.
+void test_5mzjwm9llcksb1og()
+{
+	auto is_failed = false;
+
+	try
+	{
+		bstone::make_sha1_digest("b6589fc6ab0dc82cf12099d1c2d40aB994E8410C", 3);
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(is_failed);
+}
+
+// constexpr Sha1Digest make_sha1_digest(const TChar*, std::intptr_t)
+// char
+// Invalid string.
+void test_osnn3bobbkydx33t()
+{
+	auto is_failed = false;
+
+	try
+	{
+		bstone::make_sha1_digest("????????????????????????????????????????", bstone::sha1_digest_char_count);
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(is_failed);
+}
+
+// ==========================================================================
+
+// constexpr Sha1Digest make_sha1_digest(const TChar*, std::intptr_t)
+// wchar_t
+void test_0i4ic6gnbw9z4tij()
+{
+	constexpr auto ref_digest = bstone::Sha1Digest
+	{
+		std::uint8_t{0xB6}, std::uint8_t{0x58}, std::uint8_t{0x9F}, std::uint8_t{0xC6}, std::uint8_t{0xAB},
+		std::uint8_t{0x0D}, std::uint8_t{0xC8}, std::uint8_t{0x2C}, std::uint8_t{0xF1}, std::uint8_t{0x20},
+		std::uint8_t{0x99}, std::uint8_t{0xD1}, std::uint8_t{0xC2}, std::uint8_t{0xD4}, std::uint8_t{0x0A},
+		std::uint8_t{0xB9}, std::uint8_t{0x94}, std::uint8_t{0xE8}, std::uint8_t{0x41}, std::uint8_t{0x0C},
+	};
+
+	constexpr auto digest = bstone::make_sha1_digest(
+		L"b6589fc6ab0dc82cf12099d1c2d40aB994E8410C???", bstone::sha1_digest_char_count);
+
+	constexpr auto is_valid = digest == ref_digest;
+
+	tester.check(is_valid);
+}
+
+// constexpr Sha1Digest make_sha1_digest(const TChar*, std::intptr_t)
+// char
+// Invalid string length.
+void test_p4yyydr8g9eoyunn()
+{
+	auto is_failed = false;
+
+	try
+	{
+		bstone::make_sha1_digest(L"b6589fc6ab0dc82cf12099d1c2d40aB994E8410C", 3);
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(is_failed);
+}
+
+// constexpr Sha1Digest make_sha1_digest(const TChar*, std::intptr_t)
+// char
+// Invalid string.
+void test_z5zvdoi7mnfotb62()
+{
+	auto is_failed = false;
+
+	try
+	{
+		bstone::make_sha1_digest(L"????????????????????????????????????????", bstone::sha1_digest_char_count);
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(is_failed);
+}
+
+// ==========================================================================
+
+// Sha1Digest make_sha1_digest(const TChar*)
+// char
+void test_ifkd3ng0v38k7fhx()
+{
+	constexpr auto ref_digest = bstone::Sha1Digest
+	{
+		std::uint8_t{0xB6}, std::uint8_t{0x58}, std::uint8_t{0x9F}, std::uint8_t{0xC6}, std::uint8_t{0xAB},
+		std::uint8_t{0x0D}, std::uint8_t{0xC8}, std::uint8_t{0x2C}, std::uint8_t{0xF1}, std::uint8_t{0x20},
+		std::uint8_t{0x99}, std::uint8_t{0xD1}, std::uint8_t{0xC2}, std::uint8_t{0xD4}, std::uint8_t{0x0A},
+		std::uint8_t{0xB9}, std::uint8_t{0x94}, std::uint8_t{0xE8}, std::uint8_t{0x41}, std::uint8_t{0x0C},
+	};
+
+	constexpr auto digest = bstone::make_sha1_digest("b6589fc6ab0dc82cf12099d1c2d40aB994E8410C");
+	constexpr auto is_valid = digest == ref_digest;
+
+	tester.check(is_valid);
+}
+
+// Sha1Digest make_sha1_digest(const TChar*)
+// char
+// Invalid string.
+void test_oxsseq12zq4gc0mh()
+{
+	auto is_failed = false;
+
+	try
+	{
+		bstone::make_sha1_digest("????????????????????????????????????????");
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(is_failed);
+}
+
+// ==========================================================================
+
+// Sha1Digest make_sha1_digest(const TChar*)
+// wchar_t
+void test_de36ayiiisb95y67()
+{
+	constexpr auto ref_digest = bstone::Sha1Digest
+	{
+		std::uint8_t{0xB6}, std::uint8_t{0x58}, std::uint8_t{0x9F}, std::uint8_t{0xC6}, std::uint8_t{0xAB},
+		std::uint8_t{0x0D}, std::uint8_t{0xC8}, std::uint8_t{0x2C}, std::uint8_t{0xF1}, std::uint8_t{0x20},
+		std::uint8_t{0x99}, std::uint8_t{0xD1}, std::uint8_t{0xC2}, std::uint8_t{0xD4}, std::uint8_t{0x0A},
+		std::uint8_t{0xB9}, std::uint8_t{0x94}, std::uint8_t{0xE8}, std::uint8_t{0x41}, std::uint8_t{0x0C},
+	};
+
+	constexpr auto digest = bstone::make_sha1_digest(L"b6589fc6ab0dc82cf12099d1c2d40aB994E8410C");
+	constexpr auto is_valid = digest == ref_digest;
+	tester.check(is_valid);
+}
+
+// Sha1Digest make_sha1_digest(const TChar*)
+// char
+// Invalid string.
+void test_fzf597nw6linsoxi()
+{
+	auto is_failed = false;
+
+	try
+	{
+		bstone::make_sha1_digest(L"????????????????????????????????????????", bstone::sha1_digest_char_count);
+	}
+	catch (...)
+	{
+		is_failed = true;
+	}
+
+	tester.check(is_failed);
+}
+
+// ==========================================================================
+
 class Registrator
 {
 public:
 	Registrator()
 	{
 		register_sha1();
+		register_make_sha1_digest();
 	}
 
 private:
@@ -169,6 +367,23 @@ private:
 		tester.register_test("Sha1#lpjru7shzc0ctgsw", test_lpjru7shzc0ctgsw);
 		tester.register_test("Sha1#pmq9ub20p7haxiyv", test_pmq9ub20p7haxiyv);
 		tester.register_test("Sha1#hka86bzux5vmorkw", test_hka86bzux5vmorkw);
+	}
+
+	void register_make_sha1_digest()
+	{
+		tester.register_test("make_sha1_digest#0km1jccde7pdny02", test_0km1jccde7pdny02);
+		tester.register_test("make_sha1_digest#5mzjwm9llcksb1og", test_5mzjwm9llcksb1og);
+		tester.register_test("make_sha1_digest#osnn3bobbkydx33t", test_osnn3bobbkydx33t);
+
+		tester.register_test("make_sha1_digest#0i4ic6gnbw9z4tij", test_0i4ic6gnbw9z4tij);
+		tester.register_test("make_sha1_digest#p4yyydr8g9eoyunn", test_p4yyydr8g9eoyunn);
+		tester.register_test("make_sha1_digest#z5zvdoi7mnfotb62", test_z5zvdoi7mnfotb62);
+
+		tester.register_test("make_sha1_digest#ifkd3ng0v38k7fhx", test_ifkd3ng0v38k7fhx);
+		tester.register_test("make_sha1_digest#oxsseq12zq4gc0mh", test_oxsseq12zq4gc0mh);
+
+		tester.register_test("make_sha1_digest#de36ayiiisb95y67", test_de36ayiiisb95y67);
+		tester.register_test("make_sha1_digest#fzf597nw6linsoxi", test_fzf597nw6linsoxi);
 	}
 };
 
