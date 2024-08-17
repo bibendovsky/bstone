69d6a5f1e9e1e7393de4e1822ae83ba50ca6e661
Boris I. Bendovsky <bibendovsky@hotmail.com>
2024-08-17 13:03:05 +0300

    Fix C++ feature detection

--- src/bstone_cxx.h
+++ src/bstone_cxx.h
@@ -1,6 +1,6 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2023 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2023-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
@@ -10,7 +10,7 @@ SPDX-License-Identifier: MIT
 #define BSTONE_CXX_INCLUDED
 
 #ifndef BSTONE_CXX_NODISCARD
-	#if __cplusplus == 201703L
+	#if __cplusplus >= 201703L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
 		#define BSTONE_CXX_NODISCARD [[nodiscard]]
 	#else
 		#define BSTONE_CXX_NODISCARD
