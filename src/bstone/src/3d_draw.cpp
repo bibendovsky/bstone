a892126583515761379423942298cf191411aa4c
Boris I. Bendovsky <bibendovsky@hotmail.com>
2024-08-30 22:14:32 +0300

    Fix direction range in CalcRotate

--- src/bstone/src/3d_draw.cpp
+++ src/bstone/src/3d_draw.cpp
@@ -20,6 +20,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include "id_vh.h"
 #include "id_vl.h"
 
+#include "bstone_algorithm.h"
 #include "bstone_door.h"
 #include "bstone_generic_fizzle_fx.h"
 #include "bstone_globals.h"
@@ -1034,7 +1035,7 @@ std::int16_t CalcRotate(
 
 	if (dir == nodir)
 	{
-		dir = static_cast<dirtype>(ob->trydir & 127);
+		dir = static_cast<dirtype>(bstone::clamp(ob->trydir & 127, 0, 8));
 	}
 
 	int target_angle = (view_angle - 180) - dirangle[dir];
