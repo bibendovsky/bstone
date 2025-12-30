0b5cd6da626e2828c8565005534fcc0467656a81
Boris I. Bendovsky <bibendovsky@hotmail.com>
2025-12-30 15:17:19 +0200

    Revise sys::SdlPollingAudioDevice

--- src/bstone/src/bstone_sys_polling_audio_device_sdl.cpp
+++ src/bstone/src/bstone_sys_polling_audio_device_sdl.cpp
@@ -4,30 +4,28 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-#include <cassert>
-#include <cstring>
-#include <vector>
-#include "SDL3/SDL_audio.h"
-#include "bstone_single_pool_resource.h"
-#include "bstone_sys_exception_sdl.h"
+// Polling audio device (SDL)
+
 #include "bstone_sys_polling_audio_device_sdl.h"
 #include "bstone_exception.h"
+#include "bstone_scope_exit.h"
+#include <cstddef>
+#include <format>
+#include <string>
+#include <vector>
+#include "SDL3/SDL_audio.h"
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
 namespace {
 
-class SdlPollingAudioDevice final : public PollingAudioDevice
+class PollingAudioDeviceSdl final : public PollingAudioDevice
 {
 public:
-	SdlPollingAudioDevice(Logger& logger, const PollingAudioDeviceOpenParam& param);
-	SdlPollingAudioDevice(const SdlPollingAudioDevice&) = delete;
-	SdlPollingAudioDevice& operator=(const SdlPollingAudioDevice&) = delete;
-	~SdlPollingAudioDevice() override;
-
-	void* operator new(std::size_t size);
-	void operator delete(void* ptr);
+	PollingAudioDeviceSdl(Logger& logger, const PollingAudioDeviceOpenParam& param);
+	PollingAudioDeviceSdl(const PollingAudioDeviceSdl&) = delete;
+	PollingAudioDeviceSdl& operator=(const PollingAudioDeviceSdl&) = delete;
+	~PollingAudioDeviceSdl() override;
 
 private:
 	using Sample = float;
@@ -46,128 +44,121 @@ private:
 	int audio_cache_size_{};
 	int audio_cache_offset_{};
 
-private:
 	int do_get_rate() const noexcept override;
 	int do_get_channel_count() const noexcept override;
 	int do_get_frame_count() const noexcept override;
 
 	void do_pause(bool is_pause) override;
 
-private:
-	static MemoryResource& get_memory_resource();
-
-	static void SDLCALL sdl_callback(void* userdata, SDL_AudioStream* stream, int additional_amount, int total_amount);
+	static void SDLCALL callback_proxy(void* userdata, SDL_AudioStream* stream, int additional_amount, int total_amount);
 	void callback(int sdl_size);
 };
 
-// ==========================================================================
+// --------------------------------------
 
-SdlPollingAudioDevice::SdlPollingAudioDevice(Logger& logger, const PollingAudioDeviceOpenParam& param)
-try
+PollingAudioDeviceSdl::PollingAudioDeviceSdl(Logger& logger, const PollingAudioDeviceOpenParam& param)
 	:
 	logger_{logger}
 {
-	logger_.log_information("<<< Start up SDL polling audio device.");
-
+	logger_.log_information("Starting SDL polling audio device.");
 	if (param.channel_count <= 0 || param.channel_count > 255)
 	{
 		BSTONE_THROW_STATIC_SOURCE("Channel count out of range.");
 	}
-
 	if (param.desired_frame_count <= 0 || param.desired_frame_count > 65535)
 	{
 		BSTONE_THROW_STATIC_SOURCE("Frame count out of range.");
 	}
-
 	if (param.callback == nullptr)
 	{
 		BSTONE_THROW_STATIC_SOURCE("Null callback.");
 	}
-
 	callback_ = param.callback;
-
 	const SDL_AudioSpec sdl_audio_spec{
 		/* format   */ SDL_AUDIO_F32,
 		/* channels */ param.channel_count,
 		/* freq     */ param.desired_rate,
 	};
-	sdl_audio_stream_ = sdl_ensure_result(SDL_OpenAudioDeviceStream(
+	SDL_AudioStream* sdl_audio_stream = SDL_OpenAudioDeviceStream(
 		SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK,
 		&sdl_audio_spec,
-		sdl_callback,
-		this));
+		callback_proxy,
+		this);
+	if (sdl_audio_stream == nullptr)
+	{
+		const std::string message = std::format("[{}] {}", "SDL_OpenAudioDeviceStream", SDL_GetError());
+		BSTONE_THROW_DYNAMIC_SOURCE(message.c_str());
+	}
+	const auto scope_exit = make_scope_exit(
+		[&sdl_audio_stream]()
+		{
+			if (sdl_audio_stream != nullptr)
+			{
+				SDL_DestroyAudioStream(sdl_audio_stream);
+			}
+		});
 	rate_ = param.desired_rate;
 	channel_count_ = param.channel_count;
 	frame_count_ = param.desired_frame_count;
 	audio_cache_capacity_ = sample_size * channel_count_ * frame_count_;
 	audio_cache_.resize(static_cast<std::size_t>(audio_cache_capacity_));
-	logger_.log_information(">>> SDL polling audio device started up.");
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+	logger_.log_information("SDL polling audio device has started.");
+	sdl_audio_stream_ = sdl_audio_stream;
+	sdl_audio_stream = nullptr;
+}
 
-SdlPollingAudioDevice::~SdlPollingAudioDevice()
+PollingAudioDeviceSdl::~PollingAudioDeviceSdl()
 {
 	logger_.log_information("Shut down SDL polling audio device.");
-	if (sdl_audio_stream_ != nullptr)
-	{
-		SDL_DestroyAudioStream(sdl_audio_stream_);
-	}
+	SDL_DestroyAudioStream(sdl_audio_stream_);
 }
 
-void* SdlPollingAudioDevice::operator new(std::size_t size)
-try {
-	return get_memory_resource().allocate(size);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-void SdlPollingAudioDevice::operator delete(void* ptr)
-{
-	get_memory_resource().deallocate(ptr);
-}
-
-int SdlPollingAudioDevice::do_get_rate() const noexcept
+int PollingAudioDeviceSdl::do_get_rate() const noexcept
 {
 	return rate_;
 }
 
-int SdlPollingAudioDevice::do_get_channel_count() const noexcept
+int PollingAudioDeviceSdl::do_get_channel_count() const noexcept
 {
 	return channel_count_;
 }
 
-int SdlPollingAudioDevice::do_get_frame_count() const noexcept
+int PollingAudioDeviceSdl::do_get_frame_count() const noexcept
 {
 	return frame_count_;
 }
 
-void SdlPollingAudioDevice::do_pause(bool is_pause)
+void PollingAudioDeviceSdl::do_pause(bool is_pause)
 {
 	if (is_pause)
 	{
-		SDL_PauseAudioStreamDevice(sdl_audio_stream_);
+		if (!SDL_PauseAudioStreamDevice(sdl_audio_stream_))
+		{
+			const std::string message = std::format("[{}] {}", "SDL_PauseAudioStreamDevice", SDL_GetError());
+			BSTONE_THROW_DYNAMIC_SOURCE(message.c_str());
+		}
 	}
 	else
 	{
-		SDL_ResumeAudioStreamDevice(sdl_audio_stream_);
+		if (!SDL_ResumeAudioStreamDevice(sdl_audio_stream_))
+		{
+			const std::string message = std::format("[{}] {}", "SDL_ResumeAudioStreamDevice", SDL_GetError());
+			BSTONE_THROW_DYNAMIC_SOURCE(message.c_str());
+		}
 	}
 }
 
-MemoryResource& SdlPollingAudioDevice::get_memory_resource()
-{
-	static SinglePoolResource<SdlPollingAudioDevice> memory_pool{};
-
-	return memory_pool;
-}
-
-void SDLCALL SdlPollingAudioDevice::sdl_callback(
+void SDLCALL PollingAudioDeviceSdl::callback_proxy(
 	void* userdata,
 	[[maybe_unused]] SDL_AudioStream* stream,
 	[[maybe_unused]] int additional_amount,
 	int total_amount)
 {
-	static_cast<SdlPollingAudioDevice*>(userdata)->callback(total_amount);
+	static_cast<PollingAudioDeviceSdl*>(userdata)->callback(total_amount);
 }
 
-void SdlPollingAudioDevice::callback(int sdl_size)
-try {
+void PollingAudioDeviceSdl::callback(int sdl_size)
+{
 	if (sdl_size <= 0)
 	{
 		return;
@@ -189,16 +180,15 @@ try {
 		sdl_offset += put_count;
 		audio_cache_offset_ += put_count;
 	}
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+}
 
 } // namespace
 
-// ==========================================================================
+// ======================================
 
-PollingAudioDeviceUPtr make_sdl_polling_audio_device(Logger& logger, const PollingAudioDeviceOpenParam& param)
-try {
-	return std::make_unique<SdlPollingAudioDevice>(logger, param);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+PollingAudioDeviceUPtr make_polling_audio_device_sdl(Logger& logger, const PollingAudioDeviceOpenParam& param)
+{
+	return std::make_unique<PollingAudioDeviceSdl>(logger, param);
+}
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
