4007fb4d1c374acbc1a20c5253e9f37fea63802e
Boris I. Bendovsky <bibendovsky@hotmail.com>
2025-12-30 14:59:53 +0200

    Revise sys::SdlMouseMgr

--- src/bstone/src/bstone_sys_mouse_mgr_sdl.cpp
+++ src/bstone/src/bstone_sys_mouse_mgr_sdl.cpp
@@ -4,69 +4,46 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
+// Mouse manager (SDL)
+
+#include "bstone_exception.h"
+#include "bstone_scope_exit.h"
+#include "bstone_sys_mouse_mgr_sdl.h"
+#include <format>
+#include <string>
 #include "SDL3/SDL_mouse.h"
 #include "SDL3/SDL_video.h"
-#include "bstone_exception.h"
-#include "bstone_single_pool_resource.h"
-#include "bstone_sys_exception_sdl.h"
-#include "bstone_sys_mouse_mgr_sdl.h"
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
 namespace {
 
-class SdlMouseMgr final : public MouseMgr
+class MouseMgrSdl final : public MouseMgr
 {
 public:
-	SdlMouseMgr(Logger& logger);
-	~SdlMouseMgr() override;
-
-	void* operator new(std::size_t size);
-	void operator delete(void* ptr);
-
-private:
-	Logger& logger_;
+	MouseMgrSdl() = default;
+	MouseMgrSdl(const MouseMgrSdl&) = delete;
+	MouseMgrSdl& operator=(const MouseMgrSdl&) = delete;
+	~MouseMgrSdl() override = default;
 
 private:
 	void do_set_relative_mode(bool is_enable) override;
-
-private:
-	static MemoryResource& get_memory_resource();
 };
 
-// ==========================================================================
+// --------------------------------------
 
-SdlMouseMgr::SdlMouseMgr(Logger& logger)
-try
-	:
-	logger_{logger}
-{
-	logger_.log_information("Start up SDL mouse manager.");
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-SdlMouseMgr::~SdlMouseMgr()
-{
-	logger_.log_information("Shut down SDL mouse manager.");
-}
-
-void* SdlMouseMgr::operator new(std::size_t size)
-try {
-	return get_memory_resource().allocate(size);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-void SdlMouseMgr::operator delete(void* ptr)
-{
-	get_memory_resource().deallocate(ptr);
-}
-
-void SdlMouseMgr::do_set_relative_mode(bool is_enable)
+void MouseMgrSdl::do_set_relative_mode(bool is_enable)
 {
 	// TODO Move it somewhere else.
 	int window_count;
 	SDL_Window** const sdl_windows = SDL_GetWindows(&window_count);
 	if (sdl_windows != nullptr)
 	{
+		const auto scope_exit = make_scope_exit(
+			[sdl_windows]()
+			{
+				SDL_free(sdl_windows);
+			});
 		if (window_count > 0)
 		{
 			if (window_count != 1)
@@ -74,27 +51,25 @@ void SdlMouseMgr::do_set_relative_mode(bool is_enable)
 				BSTONE_THROW_STATIC_SOURCE("Too many windows.");
 			}
 			SDL_Window* const sdl_window = sdl_windows[0];
-			SDL_SetWindowRelativeMouseMode(sdl_window, is_enable);
+			if (!SDL_SetWindowRelativeMouseMode(sdl_window, is_enable))
+			{
+				const std::string message = std::format(
+					"[{}] {}",
+					"SDL_SetWindowRelativeMouseMode",
+					SDL_GetError());
+				BSTONE_THROW_DYNAMIC_SOURCE(message.c_str());
+			}
 		}
-		SDL_free(sdl_windows);
 	}
-}
-
-MemoryResource& SdlMouseMgr::get_memory_resource()
-{
-	static SinglePoolResource<SdlMouseMgr> memory_pool{};
-
-	return memory_pool;
 }
 
 } // namespace
 
-// ==========================================================================
+// ======================================
 
-MouseMgrUPtr make_sdl_mouse_mgr(Logger& logger)
+MouseMgrUPtr make_mouse_mgr_sdl([[maybe_unused]] Logger& logger)
 {
-	return std::make_unique<SdlMouseMgr>(logger);
+	return std::make_unique<MouseMgrSdl>();
 }
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
