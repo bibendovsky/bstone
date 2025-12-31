662f6088a15c46d8ab247641bdb4d13f5e91b8ec
Boris I. Bendovsky <bibendovsky@hotmail.com>
2025-12-31 20:48:59 +0200

    Revise sys::SdlTextureLock

--- src/bstone/src/bstone_sys_texture_lock_sdl.h
+++ src/bstone/src/bstone_sys_texture_lock_sdl.h
@@ -4,21 +4,20 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
+// Texture lock (SDL)
+
 #ifndef BSTONE_SYS_TEXTURE_LOCK_SDL_INCLUDED
 #define BSTONE_SYS_TEXTURE_LOCK_SDL_INCLUDED
 
 #include "bstone_sys_rectangle.h"
 #include "bstone_sys_texture_sdl.h"
 #include "bstone_sys_texture_lock_sdl.h"
+#include "SDL3/SDL_render.h"
 
-struct SDL_Texture;
+namespace bstone::sys {
 
-namespace bstone {
-namespace sys {
+TextureLockUPtr make_texture_lock_sdl(SDL_Texture& sdl_texture, const Rectangle* rect);
 
-TextureLockUPtr make_sdl_texture_lock(SDL_Texture& sdl_texture, const Rectangle* rect);
-
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
 
 #endif // BSTONE_SYS_TEXTURE_LOCK_SDL_INCLUDED
