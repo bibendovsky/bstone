c176b601383810bec8edd5e50b9d2cc77b850a20
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-01 17:29:31 +0200

    Revise sys::SdlWindowRoundedCornerMgr

--- src/bstone/src/bstone_sys_window_rounded_corner_mgr_sdl.cpp
+++ src/bstone/src/bstone_sys_window_rounded_corner_mgr_sdl.cpp
@@ -13,16 +13,11 @@ SPDX-License-Identifier: MIT
 #endif
 
 #include "bstone_sys_window_rounded_corner_mgr.h"
-
-#include <cstddef>
-
-#include <windows.h>
-
 #include "bstone_exception.h"
-#include "bstone_single_pool_resource.h"
 #include "bstone_shared_library.h"
 #include "bstone_sys_window_sdl.h"
 #include "bstone_win32_os_version.h"
+#include <windows.h>
 
 enum DWMWINDOWATTRIBUTE
 {
@@ -37,21 +32,17 @@ enum DWM_WINDOW_CORNER_PREFERENCE
 	DWMWCP_ROUNDSMALL = 3
 };
 
-// ==========================================================================
+// ======================================
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
 namespace {
 
-class SdlWindowRoundedCornerMgr final : public WindowRoundedCornerMgr
+class WindowRoundedCornerMgrSdl final : public WindowRoundedCornerMgr
 {
 public:
-	SdlWindowRoundedCornerMgr();
-	~SdlWindowRoundedCornerMgr() override;
-
-	void* operator new(std::size_t size);
-	void operator delete(void* ptr) noexcept;
+	WindowRoundedCornerMgrSdl();
+	~WindowRoundedCornerMgrSdl() override = default;
 
 private:
 	using PFNDWMSETWINDOWATTRIBUTEPROC = HRESULT (WINAPI *)(
@@ -60,66 +51,36 @@ private:
 		LPCVOID pvAttribute,
 		DWORD cbAttribute);
 
-	using Pool = SinglePoolResource<SdlWindowRoundedCornerMgr>;
-
-private:
 	void do_set_round_corner_type(Window& window, WindowRoundedCornerType rounded_corner_type) override;
 
-private:
-	static Pool pool_;
-
-private:
 	SharedLibrary wdmapi_shared_library_{};
 	PFNDWMSETWINDOWATTRIBUTEPROC DwmSetWindowAttribute_{};
 };
 
-// --------------------------------------------------------------------------
+// --------------------------------------
 
-SdlWindowRoundedCornerMgr::Pool SdlWindowRoundedCornerMgr::pool_{};
-
-// --------------------------------------------------------------------------
-
-SdlWindowRoundedCornerMgr::SdlWindowRoundedCornerMgr()
+WindowRoundedCornerMgrSdl::WindowRoundedCornerMgrSdl()
 {
-	constexpr auto min_os_version = win32::OsVersion{10, 0, 22000};
-	const auto os_version = win32::get_os_version();
-
+	constexpr win32::OsVersion min_os_version{10, 0, 22000};
+	const win32::OsVersion os_version = win32::get_os_version();
 	if (os_version < min_os_version)
 	{
 		return;
 	}
-
-	auto wdmapi_shared_library = SharedLibrary{};
-
+	SharedLibrary wdmapi_shared_library{};
 	if (!wdmapi_shared_library.try_open("dwmapi.dll"))
 	{
 		return;
 	}
-
-	DwmSetWindowAttribute_ = wdmapi_shared_library.find_symbol<PFNDWMSETWINDOWATTRIBUTEPROC>(
-		"DwmSetWindowAttribute");
-
+	DwmSetWindowAttribute_ = wdmapi_shared_library.find_symbol<PFNDWMSETWINDOWATTRIBUTEPROC>("DwmSetWindowAttribute");
 	if (DwmSetWindowAttribute_ == nullptr)
 	{
 		return;
 	}
-
 	wdmapi_shared_library_.swap(wdmapi_shared_library);
 }
 
-SdlWindowRoundedCornerMgr::~SdlWindowRoundedCornerMgr() = default;
-
-void* SdlWindowRoundedCornerMgr::operator new(std::size_t size)
-{
-	return pool_.allocate(size);
-}
-
-void SdlWindowRoundedCornerMgr::operator delete(void* ptr) noexcept
-{
-	pool_.deallocate(ptr);
-}
-
-void SdlWindowRoundedCornerMgr::do_set_round_corner_type(
+void WindowRoundedCornerMgrSdl::do_set_round_corner_type(
 	Window& window,
 	WindowRoundedCornerType rounded_corner_type)
 {
@@ -127,27 +88,31 @@ void SdlWindowRoundedCornerMgr::do_set_round_corner_type(
 	{
 		return;
 	}
-
-	auto win32_rounded_corner_type = DWORD{};
-
+	DWORD win32_rounded_corner_type = 0;
 	switch (rounded_corner_type)
 	{
-		case WindowRoundedCornerType::none: win32_rounded_corner_type = DWMWCP_DONOTROUND; break;
-		case WindowRoundedCornerType::system: win32_rounded_corner_type = DWMWCP_DEFAULT; break;
-		case WindowRoundedCornerType::round: win32_rounded_corner_type = DWMWCP_ROUND; break;
-		case WindowRoundedCornerType::round_small: win32_rounded_corner_type = DWMWCP_ROUNDSMALL; break;
-		default: BSTONE_THROW_STATIC_SOURCE("Unknown window rounded corner type.");
+		case WindowRoundedCornerType::none:
+			win32_rounded_corner_type = DWMWCP_DONOTROUND;
+			break;
+		case WindowRoundedCornerType::system:
+			win32_rounded_corner_type = DWMWCP_DEFAULT;
+			break;
+		case WindowRoundedCornerType::round:
+			win32_rounded_corner_type = DWMWCP_ROUND;
+			break;
+		case WindowRoundedCornerType::round_small:
+			win32_rounded_corner_type = DWMWCP_ROUNDSMALL;
+			break;
+		default:
+			BSTONE_THROW_STATIC_SOURCE("Unknown window rounded corner type.");
 	}
-
-	const auto native_handle = static_cast<SdlWindowInternal&>(window).get_native_handle();
-	const auto win32_native_handle = static_cast<HWND>(native_handle);
-
-	const auto hresult = DwmSetWindowAttribute_(
+	void* const native_handle = static_cast<SdlWindowInternal&>(window).get_native_handle();
+	const HWND win32_native_handle = static_cast<HWND>(native_handle);
+	const HRESULT hresult = DwmSetWindowAttribute_(
 		win32_native_handle,
 		DWMWA_WINDOW_CORNER_PREFERENCE,
 		&win32_rounded_corner_type,
-		static_cast<DWORD>(sizeof(win32_rounded_corner_type)));
-
+		sizeof(win32_rounded_corner_type));
 	if (FAILED(hresult))
 	{
 		BSTONE_THROW_STATIC_SOURCE("Failed to set window rounded corner type.");
@@ -156,14 +121,13 @@ void SdlWindowRoundedCornerMgr::do_set_round_corner_type(
 
 } // namespace
 
-// ==========================================================================
+// ======================================
 
 WindowRoundedCornerMgrUPtr make_window_rounded_corner_mgr()
 {
-	return std::make_unique<SdlWindowRoundedCornerMgr>();
+	return std::make_unique<WindowRoundedCornerMgrSdl>();
 }
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
 
 #endif // _WIN32
