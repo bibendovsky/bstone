c8b21bb21e74bac1f2f1f479828b386de4290d2b
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 09:59:59 +0200

    Revise bstone_sys_display_mode

--- src/bstone/src/bstone_sys_display_mode.h
+++ src/bstone/src/bstone_sys_display_mode.h
@@ -1,8 +1,15 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+SPDX-License-Identifier: MIT
+*/
+
 #ifndef BSTONE_SYS_DISPLAY_MODE_INCLUDED
 #define BSTONE_SYS_DISPLAY_MODE_INCLUDED
 
-namespace bstone {
-namespace sys {
+// Display mode
+
+namespace bstone::sys {
 
 struct DisplayMode
 {
@@ -11,7 +18,6 @@ struct DisplayMode
 	int refresh_rate;
 };
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
 
 #endif // BSTONE_SYS_DISPLAY_MODE_INCLUDED
