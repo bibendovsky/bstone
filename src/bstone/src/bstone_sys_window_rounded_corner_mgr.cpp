7ab28cc0ff7f0500958ee522e3f73ed36c6bf5f3
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 13:29:12 +0200

    Revise bstone_sys_window_rounded_corner_mgr

--- src/bstone/src/bstone_sys_window_rounded_corner_mgr.cpp
+++ src/bstone/src/bstone_sys_window_rounded_corner_mgr.cpp
@@ -4,21 +4,15 @@ Copyright (c) 2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
-// Window rounded corner manager.
+// Window rounded corner manager
 
 #include "bstone_sys_window_rounded_corner_mgr.h"
 
-namespace bstone {
-namespace sys {
-
-WindowRoundedCornerMgr::WindowRoundedCornerMgr() = default;
-
-WindowRoundedCornerMgr::~WindowRoundedCornerMgr() = default;
+namespace bstone::sys {
 
 void WindowRoundedCornerMgr::set_round_corner_type(Window& window, WindowRoundedCornerType round_corner_type)
 {
 	do_set_round_corner_type(window, round_corner_type);
 }
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
