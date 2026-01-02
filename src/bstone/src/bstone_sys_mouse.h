13875d609073f70708ab13db39481d7ecc6d2d88
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 10:56:47 +0200

    Revise bstone_sys_mouse

--- src/bstone/src/bstone_sys_mouse.h
+++ src/bstone/src/bstone_sys_mouse.h
@@ -4,13 +4,12 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// Mouse.
+// Mouse
 
 #ifndef BSTONE_SYS_MOUSE_INCLUDED
 #define BSTONE_SYS_MOUSE_INCLUDED
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
 enum class MouseWheelDirection
 {
@@ -21,24 +20,23 @@ enum class MouseWheelDirection
 
 struct MouseButtonIndex
 {
-	static constexpr auto left = 0;
-	static constexpr auto right = 1;
-	static constexpr auto middle = 2;
-	static constexpr auto x1 = 3;
-	static constexpr auto x2 = 4;
+	constexpr static int left = 0;
+	constexpr static int right = 1;
+	constexpr static int middle = 2;
+	constexpr static int x1 = 3;
+	constexpr static int x2 = 4;
 };
 
 struct MouseButtonMask
 {
-	static constexpr auto none = 0U;
-	static constexpr auto left = 1U << MouseButtonIndex::left;
-	static constexpr auto right = 1U << MouseButtonIndex::right;
-	static constexpr auto middle = 1U << MouseButtonIndex::middle;
-	static constexpr auto x1 = 1U << MouseButtonIndex::x1;
-	static constexpr auto x2 = 1U << MouseButtonIndex::x2;
+	constexpr static unsigned int none = 0U;
+	constexpr static unsigned int left = 1U << MouseButtonIndex::left;
+	constexpr static unsigned int right = 1U << MouseButtonIndex::right;
+	constexpr static unsigned int middle = 1U << MouseButtonIndex::middle;
+	constexpr static unsigned int x1 = 1U << MouseButtonIndex::x1;
+	constexpr static unsigned int x2 = 1U << MouseButtonIndex::x2;
 };
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
 
 #endif // BSTONE_SYS_MOUSE_INCLUDED
