7d37e96d2ab626f752a44ff655836e1e1c44aa79
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 10:29:37 +0200

    Revise bstone_sys_gl_context_profile

--- src/bstone/src/bstone_sys_gl_context_profile.cpp
+++ src/bstone/src/bstone_sys_gl_context_profile.cpp
@@ -4,6 +4,6 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// OpenGL context profile.
+// OpenGL context profile
 
 #include "bstone_sys_gl_context_profile.h"
