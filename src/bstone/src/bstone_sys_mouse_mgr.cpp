a0589f4d50fa1104de6a7c355837292640475f40
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 11:00:03 +0200

    Revise bstone_sys_mouse_mgr

--- src/bstone/src/bstone_sys_mouse_mgr.cpp
+++ src/bstone/src/bstone_sys_mouse_mgr.cpp
@@ -4,21 +4,15 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// Mouse manager.
+// Mouse manager
 
 #include "bstone_sys_mouse_mgr.h"
 
-namespace bstone {
-namespace sys {
-
-MouseMgr::MouseMgr() = default;
-
-MouseMgr::~MouseMgr() = default;
+namespace bstone::sys {
 
 void MouseMgr::set_relative_mode(bool is_relative)
 {
 	do_set_relative_mode(is_relative);
 }
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
