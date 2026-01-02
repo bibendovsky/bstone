1d1a6d42f09903987220367a8f879056eca4f004
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 09:59:59 +0200

    Revise bstone_sys_display_mode

--- src/bstone/src/bstone_sys_display_mode.cpp
+++ src/bstone/src/bstone_sys_display_mode.cpp
@@ -1,1 +1,9 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+SPDX-License-Identifier: MIT
+*/
+
+// Display mode
+
 #include "bstone_sys_display_mode.h"
