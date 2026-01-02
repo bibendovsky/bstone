4acb42debd3ceb0f6c02c356ec5c0e1db6d5c892
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 09:44:43 +0200

    Revise bstone_sys_audio_mgr_null

--- src/bstone/src/bstone_sys_audio_mgr_null.h
+++ src/bstone/src/bstone_sys_audio_mgr_null.h
@@ -4,18 +4,18 @@ Copyright (c) 2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
+// Audio manager stub.
+
 #ifndef BSTONE_SYS_AUDIO_MGR_NULL_INCLUDED
 #define BSTONE_SYS_AUDIO_MGR_NULL_INCLUDED
 
 #include "bstone_sys_audio_mgr.h"
 #include "bstone_sys_logger.h"
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
 AudioMgrUPtr make_null_audio_mgr(Logger& logger);
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
 
 #endif // BSTONE_SYS_AUDIO_MGR_NULL_INCLUDED
