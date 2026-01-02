6e7db02800f5c13f79c9bc45d6b9f0b020681900
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 10:54:13 +0200

    Revise bstone_sys_message_box

--- src/bstone/src/bstone_sys_message_box.cpp
+++ src/bstone/src/bstone_sys_message_box.cpp
@@ -4,6 +4,6 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// Message box.
+// Message box
 
 #include "bstone_sys_message_box.h"
