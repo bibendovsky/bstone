57aad9980db21495139c7f0f54196908322c2fe3
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 11:05:57 +0200

    Revise bstone_sys_polling_audio_device

--- src/bstone/src/bstone_sys_polling_audio_device.cpp
+++ src/bstone/src/bstone_sys_polling_audio_device.cpp
@@ -4,39 +4,30 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// Audio device feedding via callback.
+// Audio device feeding via callback
 
 #include "bstone_sys_polling_audio_device.h"
 
-namespace bstone {
-namespace sys {
-
-PollingAudioDeviceCallback::PollingAudioDeviceCallback() = default;
-
-PollingAudioDeviceCallback::~PollingAudioDeviceCallback() = default;
+namespace bstone::sys {
 
 void PollingAudioDeviceCallback::invoke(float* samples, int sample_count)
 {
 	do_invoke(samples, sample_count);
 }
 
-// ==========================================================================
+// ======================================
 
-PollingAudioDevice::PollingAudioDevice() = default;
-
-PollingAudioDevice::~PollingAudioDevice() = default;
-
-int PollingAudioDevice::get_rate() const noexcept
+int PollingAudioDevice::get_rate() const
 {
 	return do_get_rate();
 }
 
-int PollingAudioDevice::get_channel_count() const noexcept
+int PollingAudioDevice::get_channel_count() const
 {
 	return do_get_channel_count();
 }
 
-int PollingAudioDevice::get_frame_count() const noexcept
+int PollingAudioDevice::get_frame_count() const
 {
 	return do_get_frame_count();
 }
@@ -46,5 +37,4 @@ void PollingAudioDevice::pause(bool is_pause)
 	do_pause(is_pause);
 }
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
