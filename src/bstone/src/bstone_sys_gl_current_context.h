1861f653ed6ef7ee7ec6e53f3e7151aae65c636e
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 10:34:05 +0200

    Revise bstone_sys_gl_current_context

--- src/bstone/src/bstone_sys_gl_current_context.h
+++ src/bstone/src/bstone_sys_gl_current_context.h
@@ -4,46 +4,39 @@ Copyright (c) 2023-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// OpenGL current context.
+// OpenGL current context
 
 #ifndef BSTONE_SYS_GL_CURRENT_CONTEXT_INCLUDED
 #define BSTONE_SYS_GL_CURRENT_CONTEXT_INCLUDED
 
-#include <memory>
-
 #include "bstone_sys_gl_symbol_resolver.h"
 #include "bstone_sys_swap_interval_type.h"
+#include <memory>
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
 class GlCurrentContext
 {
 public:
-	GlCurrentContext();
-	virtual ~GlCurrentContext();
+	GlCurrentContext() = default;
+	virtual ~GlCurrentContext() = default;
 
-	bool has_extension(const char* extension_name) const noexcept;
-
-	SwapIntervalType get_swap_interval() const noexcept;
+	bool has_extension(const char* extension_name) const;
+	SwapIntervalType get_swap_interval() const;
 	void set_swap_interval(SwapIntervalType swap_interval_type);
-
-	const GlSymbolResolver& get_symbol_resolver() const noexcept;
+	const GlSymbolResolver& get_symbol_resolver() const;
 
 private:
-	virtual bool do_has_extension(const char* extension_name) const noexcept = 0;
-
-	virtual SwapIntervalType do_get_swap_interval() const noexcept = 0;
+	virtual bool do_has_extension(const char* extension_name) const = 0;
+	virtual SwapIntervalType do_get_swap_interval() const = 0;
 	virtual void do_set_swap_interval(SwapIntervalType swap_interval_type) = 0;
-
-	virtual const GlSymbolResolver& do_get_symbol_resolver() const noexcept = 0;
+	virtual const GlSymbolResolver& do_get_symbol_resolver() const = 0;
 };
 
-// ==========================================================================
+// ======================================
 
 using GlCurrentContextUPtr = std::unique_ptr<GlCurrentContext>;
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
 
 #endif // BSTONE_SYS_GL_CURRENT_CONTEXT_INCLUDED
