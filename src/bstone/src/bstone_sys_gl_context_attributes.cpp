1435db3eb759d41bfff38c4d5eed71d79823b945
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 10:27:25 +0200

    Revise bstone_sys_gl_context_attributes

--- src/bstone/src/bstone_sys_gl_context_attributes.cpp
+++ src/bstone/src/bstone_sys_gl_context_attributes.cpp
@@ -4,22 +4,6 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// OpenGL context attributes.
+// OpenGL context attributes
 
 #include "bstone_sys_gl_context_attributes.h"
-
-namespace bstone {
-namespace sys {
-
-GlContextAttributes GlContextAttributes::make_default() noexcept
-{
-	auto gl_attributes = GlContextAttributes{};
-	gl_attributes.is_accelerated = true;
-	gl_attributes.profile = GlContextProfile::compatibility;
-	gl_attributes.major_version = 1;
-	gl_attributes.minor_version = 1;
-	return gl_attributes;
-}
-
-} // namespace sys
-} // namespace bstone
