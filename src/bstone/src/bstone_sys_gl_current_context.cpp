7389844f36c64917177840bfe8cdd2de1ac3fca2
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-02 10:34:05 +0200

    Revise bstone_sys_gl_current_context

--- src/bstone/src/bstone_sys_gl_current_context.cpp
+++ src/bstone/src/bstone_sys_gl_current_context.cpp
@@ -4,23 +4,18 @@ Copyright (c) 2023-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-// OpenGL current context.
+// OpenGL current context
 
 #include "bstone_sys_gl_current_context.h"
 
-namespace bstone {
-namespace sys {
+namespace bstone::sys {
 
-GlCurrentContext::GlCurrentContext() = default;
-
-GlCurrentContext::~GlCurrentContext() = default;
-
-bool GlCurrentContext::has_extension(const char* extension_name) const noexcept
+bool GlCurrentContext::has_extension(const char* extension_name) const
 {
 	return do_has_extension(extension_name);
 }
 
-SwapIntervalType GlCurrentContext::get_swap_interval() const noexcept
+SwapIntervalType GlCurrentContext::get_swap_interval() const
 {
 	return do_get_swap_interval();
 }
@@ -30,10 +25,9 @@ void GlCurrentContext::set_swap_interval(SwapIntervalType swap_interval_type)
 	do_set_swap_interval(swap_interval_type);
 }
 
-const GlSymbolResolver& GlCurrentContext::get_symbol_resolver() const noexcept
+const GlSymbolResolver& GlCurrentContext::get_symbol_resolver() const
 {
 	return do_get_symbol_resolver();
 }
 
-} // namespace sys
-} // namespace bstone
+} // namespace bstone::sys
