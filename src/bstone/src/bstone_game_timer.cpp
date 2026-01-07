98851967c9ad30c8de3e1bae137dc0a1f5d097e8
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-07 20:51:53 +0200

    Implement timer with SDL

--- src/bstone/src/bstone_game_timer.cpp
+++ src/bstone/src/bstone_game_timer.cpp
@@ -1,13 +1,10 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
 #include "bstone_game_timer.h"
-
-#include <algorithm>
-
 #include "bstone_exception.h"
 
 namespace bstone {
@@ -17,46 +14,33 @@ void GameTimer::operator=(GameTimerTicks ticks)
 	set_ticks(ticks);
 }
 
-GameTimer::GameTimer() = default;
-
 GameTimer::~GameTimer()
 {
 	stop();
 }
 
-bool GameTimer::is_started() const noexcept
+bool GameTimer::is_started() const
 {
-	return thread_.joinable();
+	return timer_.started();
 }
 
 void GameTimer::start(int frequency)
-try {
+try
+{
 	stop();
-
 	if (frequency < 10 || frequency > 100)
 	{
 		BSTONE_THROW_STATIC_SOURCE("Frequency out of range.");
 	}
-
-	mt_is_cancellation_requested_ = false;
 	set_ticks_internal(0);
-	thread_ = Thread{&GameTimer::thread_main, this, frequency};
+	const long long period_ns = 1'000'000'000LL / frequency;
+	timer_.start_ns(period_ns, &GameTimer::callback_proxy, this);
 }
-catch (...)
-{
-	stop();
-	StaticSourceException::fail_nested(BSTONE_MAKE_SOURCE_LOCATION());
-}
+BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void GameTimer::stop() noexcept
+void GameTimer::stop()
 {
-	if (!is_started())
-	{
-		return;
-	}
-
-	mt_is_cancellation_requested_ = true;
-	thread_.join();
+	timer_.stop();
 }
 
 GameTimerTicks GameTimer::get_ticks() const
@@ -95,40 +79,22 @@ void GameTimer::set_ticks_internal(GameTimerTicks ticks)
 	mt_ticks_.store(ticks, std::memory_order_relaxed);
 }
 
-void GameTimer::increase_ticks() noexcept
+void GameTimer::increase_ticks()
 {
 	mt_ticks_.fetch_add(1, std::memory_order_relaxed);
 }
 
-void GameTimer::thread_main(int frequency) noexcept
+void GameTimer::callback_proxy(void* user_data)
 {
-	const auto base_interval = ClockTicks{1000} / frequency;
-	const auto double_base_interval = base_interval * 2;
-	auto interval_counter = ClockTicks{frequency};
-
-	increase_ticks();
-	auto last_time_point = Clock::now();
-	std::this_thread::sleep_for(Milliseconds{base_interval});
-
-	while (!mt_is_cancellation_requested_)
-	{
-		increase_ticks();
-
-		interval_counter += frequency;
-		const auto extra_interval = interval_counter / 1000;
-		interval_counter %= 1000;
-
-		const auto time_point = Clock::now();
-		const auto time_diff = time_point - last_time_point;
-		last_time_point = time_point;
-		const auto last_interval = std::chrono::duration_cast<Milliseconds>(time_diff).count();
-		const auto new_interval = double_base_interval - last_interval + extra_interval;
-		const auto new_adjusted_interval = std::max(new_interval, ClockTicks{0});
-		std::this_thread::sleep_for(Milliseconds{new_adjusted_interval});
-	}
+	static_cast<GameTimer*>(user_data)->callback();
 }
 
-// ==========================================================================
+void GameTimer::callback()
+{
+	increase_ticks();
+}
+
+// ======================================
 
 void operator-=(GameTimer& a, GameTimerTicks b)
 {
