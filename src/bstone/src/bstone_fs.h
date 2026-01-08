209c8367b226e5fcb55f09db43e021075baad278
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-08 21:47:10 +0200

    Replace file system management with SDL

--- src/bstone/src/bstone_fs.h
+++ src/bstone/src/bstone_fs.h
@@ -1,18 +1,17 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2023-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2023-2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
-// File system primitives.
+// File system management
 
 #ifndef BSTONE_FS_INCLUDED
 #define BSTONE_FS_INCLUDED
 
 #include <cstdint>
 
-namespace bstone {
-namespace fs {
+namespace bstone::fs {
 
 constexpr char native_separator =
 #if defined(_WIN32)
@@ -22,12 +21,10 @@ constexpr char native_separator =
 #endif
 ;
 
-// ==========================================================================
+// ======================================
 
 std::intptr_t get_working_directory(char* buffer, std::intptr_t buffer_size);
-void set_working_directory(const char* path);
 
-void create_directory(const char* path);
 void create_directories(const char* path);
 
 void rename(const char* old_path, const char* new_path);
@@ -38,7 +35,6 @@ void remove(const char* path);
 bool is_directory_exists(const char* path);
 bool is_regular_file_exists(const char* path);
 
-} // namespace fs
-} // namespace bstone
+} // namespace bstone::fs
 
 #endif // BSTONE_FS_INCLUDED
