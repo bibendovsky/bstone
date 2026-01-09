9c2f5049a3df1c10ef5fbb59568b48cc11bf35f3
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-09 15:42:26 +0200

    Implement bstone_shared_library with SDL

--- src/bstone/src/bstone_shared_library.h
+++ src/bstone/src/bstone_shared_library.h
@@ -1,54 +1,49 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2013-2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
 #ifndef BSTONE_SHARED_LIBRARY_INCLUDED
 #define BSTONE_SHARED_LIBRARY_INCLUDED
 
-#include <memory>
+#include <bit>
 
 namespace bstone {
 
-struct SharedLibraryHandleDeleter
-{
-	void operator()(void* handle) const noexcept;
-};
-
-using SharedLibraryHandleUPtr = std::unique_ptr<void, SharedLibraryHandleDeleter>;
-
-// ==========================================================================
+using SharedLibrarySymbol = void (*)();
 
 class SharedLibrary
 {
 public:
-	SharedLibrary() noexcept;
+	SharedLibrary() = default;
 	explicit SharedLibrary(const char* file_path);
+	SharedLibrary(const SharedLibrary&) = delete;
+	SharedLibrary& operator=(const SharedLibrary&) = delete;
+	~SharedLibrary();
 
-	bool is_open() const noexcept;
+	bool is_open() const;
 	bool try_open(const char* file_path);
 	void open(const char* file_path);
-	void close() noexcept;
+	void close();
 
-	void* find_symbol(const char* symbol_name) const noexcept;
+	SharedLibrarySymbol find_symbol(const char* symbol_name);
 
 	template<typename T>
-	T find_symbol(const char* symbol_name) const noexcept;
+	T find_symbol(const char* symbol_name)
+	{
+		return std::bit_cast<T>(find_symbol(symbol_name));
+	}
 
 	void swap(SharedLibrary& rhs) noexcept;
 
 private:
-	SharedLibraryHandleUPtr handle_{};
+	class Impl;
+
+	void* native_handle_{};
+
+	void internal_close();
 };
-
-// --------------------------------------------------------------------------
-
-template<typename T>
-T SharedLibrary::find_symbol(const char* symbol_name) const noexcept
-{
-	return reinterpret_cast<T>(find_symbol(symbol_name));
-}
 
 } // namespace bstone
 
