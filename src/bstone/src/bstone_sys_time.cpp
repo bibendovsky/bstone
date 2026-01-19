b7f6491b059e453606194b9bac7ecc7dd36a6fc1
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-19 23:45:31 +0200

    Implement date/time management with SDL

---
+++ src/bstone/src/bstone_sys_time.cpp
@@ -0,0 +1,9 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+SPDX-License-Identifier: MIT
+*/
+
+// Date and time management
+
+#include "bstone_sys_time.h"
