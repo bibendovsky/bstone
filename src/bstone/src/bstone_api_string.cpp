a43b9c8e5893cfd8a5304bb40a89bf2c1d019207
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-01-31 14:46:10 +0200

    Remove bstone_api_string

--- src/bstone/src/bstone_api_string.cpp
+++
@@ -1,9 +0,0 @@
-/*
-BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2023-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
-SPDX-License-Identifier: MIT
-*/
-
-// Null-terminated API string primitive.
-
-#include "bstone_api_string.h"
