95c5b00a9ed7aeac65ac067dce70b2258ae184f8
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-01 21:45:34 +0200

    Revise bstone_entry_point

---
+++ src/bstone/src/bstone_entry_point_sdl.cpp
@@ -0,0 +1,16 @@
+/*
+BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
+Copyright (c) 2013-2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+SPDX-License-Identifier: MIT
+*/
+
+#define BSTONE_ENTRY_POINT_IMPLEMENTATION
+
+#include "bstone_entry_point.h"
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
+
+int main(int argc, char* argv[])
+{
+	return bstone_entry_point(argc, argv);
+}
