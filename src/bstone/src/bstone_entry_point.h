ac3fd5f3317d80bf13e2d799e95c18fea3ec40e8
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-01 21:45:34 +0200

    Revise bstone_entry_point

--- src/bstone/src/bstone_entry_point.h
+++ src/bstone/src/bstone_entry_point.h
@@ -1,19 +1,17 @@
 /*
 BStone: Unofficial source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike
-Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2013-2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: MIT
 */
 
 #ifndef BSTONE_ENTRY_POINT_INCLUDED
 #define BSTONE_ENTRY_POINT_INCLUDED
 
-#if defined(_WIN32)
-	#ifndef BSTONE_ENTRY_POINT_IMPLEMENTATION
-		#undef main
-		#define main bstone_entry_point
-	#endif
-
-	extern int bstone_entry_point(int argc, char** argv);
+#ifndef BSTONE_ENTRY_POINT_IMPLEMENTATION
+	#undef main
+	#define main bstone_entry_point
 #endif
+
+extern int bstone_entry_point(int argc, char* argv[]);
 
 #endif // BSTONE_ENTRY_POINT_INCLUDED
