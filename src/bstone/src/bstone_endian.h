47a3c931f8cda011dfd915fb1bc22bf4db3267ce
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-03 11:57:55 +0200

    Add non-swapping functions to bstone_endian

--- src/bstone/src/bstone_endian.h
+++ src/bstone/src/bstone_endian.h
@@ -258,6 +258,18 @@ inline constexpr T to_big(T value) noexcept
 #endif
 }
 
+std::int16_t read_s16_le(const void* buffer);
+std::uint16_t read_u16_le(const void* buffer);
+
+std::int32_t read_s32_le(const void* buffer);
+std::uint32_t read_u32_le(const void* buffer);
+
+void write_s16_le(std::int16_t value, void* buffer);
+void write_u16_le(std::uint16_t value, void* buffer);
+
+void write_s32_le(std::int32_t value, void* buffer);
+void write_u32_le(std::uint32_t value, void* buffer);
+
 } // namespace endian
 } // namespace bstone
 
