e286bcf844f6a32173fc13a4482741bbcdc1cb65
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-03 11:57:55 +0200

    Add non-swapping functions to bstone_endian

--- src/bstone/src/bstone_endian.cpp
+++ src/bstone/src/bstone_endian.cpp
@@ -7,3 +7,61 @@ SPDX-License-Identifier: MIT
 // Endianness.
 
 #include "bstone_endian.h"
+
+namespace bstone::endian {
+
+std::int16_t read_s16_le(const void* buffer)
+{
+	return static_cast<std::int16_t>(read_u16_le(buffer));
+}
+
+std::uint16_t read_u16_le(const void* buffer)
+{
+	const std::uint8_t* const bytes = static_cast<const std::uint8_t*>(buffer);
+	return static_cast<std::uint16_t>(
+		 static_cast<std::uint_fast16_t>(bytes[0])     ) |
+		(static_cast<std::uint_fast16_t>(bytes[1]) << 8);
+}
+
+std::int32_t read_s32_le(const void* buffer)
+{
+	return static_cast<std::int32_t>(read_u32_le(buffer));
+}
+
+std::uint32_t read_u32_le(const void* buffer)
+{
+	const std::uint8_t* const bytes = static_cast<const std::uint8_t*>(buffer);
+	return static_cast<std::uint32_t>(
+		 static_cast<std::uint_fast32_t>(bytes[0])      ) |
+		(static_cast<std::uint_fast32_t>(bytes[1]) <<  8) |
+		(static_cast<std::uint_fast32_t>(bytes[2]) << 16) |
+		(static_cast<std::uint_fast32_t>(bytes[3]) << 24);
+}
+
+void write_s16_le(std::int16_t value, void* buffer)
+{
+	write_u16_le(static_cast<std::uint16_t>(value), buffer);
+}
+
+void write_u16_le(std::uint16_t value, void* buffer)
+{
+	std::uint8_t* const bytes = static_cast<std::uint8_t*>(buffer);
+	bytes[0] = static_cast<std::uint8_t>( static_cast<std::uint_fast16_t>(value)       & 0xFF);
+	bytes[1] = static_cast<std::uint8_t>((static_cast<std::uint_fast16_t>(value) >> 8) & 0xFF);
+}
+
+void write_s32_le(std::int32_t value, void* buffer)
+{
+	write_u32_le(static_cast<std::uint32_t>(value), buffer);
+}
+
+void write_u32_le(std::uint32_t value, void* buffer)
+{
+	std::uint8_t* const bytes = static_cast<std::uint8_t*>(buffer);
+	bytes[0] = static_cast<std::uint8_t>( static_cast<std::uint_fast32_t>(value)        & 0xFF);
+	bytes[1] = static_cast<std::uint8_t>((static_cast<std::uint_fast32_t>(value) >> 8 ) & 0xFF);
+	bytes[2] = static_cast<std::uint8_t>((static_cast<std::uint_fast32_t>(value) >> 16) & 0xFF);
+	bytes[3] = static_cast<std::uint8_t>((static_cast<std::uint_fast32_t>(value) >> 24) & 0xFF);
+}
+
+} // namespace bstone::endian
