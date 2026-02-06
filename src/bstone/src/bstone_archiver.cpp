e4a4c786aea2da890439eaccae04aba2697d423d
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-07 01:31:03 +0200

    Revise bstone_archiver

--- src/bstone/src/bstone_archiver.cpp
+++ src/bstone/src/bstone_archiver.cpp
@@ -5,102 +5,144 @@ SPDX-License-Identifier: MIT
 */
 
 #include "bstone_archiver.h"
+#include "bstone_assert.h"
+#include "bstone_crc32.h"
+#include "bstone_endian.h"
+#include "bstone_exception.h"
+#include <cstddef>
+#include <algorithm>
 
 namespace bstone {
 
 void Archiver::open(Stream& stream)
-try {
+{
 	close();
-
 	stream_ = &stream;
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+}
 
-void Archiver::close() noexcept
+void Archiver::close()
 {
 	crc32_.reset();
 	stream_ = nullptr;
 }
 
-bool Archiver::is_open() const noexcept
+bool Archiver::is_open() const
 {
 	return stream_ != nullptr;
 }
 
 bool Archiver::read_bool()
-try {
-	return read_integer<std::uint8_t>() != 0;
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	return read_uint8() != 0;
+}
 
 std::int8_t Archiver::read_int8()
-try {
-	return read_integer<std::int8_t>();
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	return static_cast<std::int8_t>(read_uint8());
+}
 
 std::uint8_t Archiver::read_uint8()
 try {
-	return read_integer<std::uint8_t>();
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 1;
+	std::uint8_t value;
+	stream_->read_exactly(&value, value_size);
+	crc32_.update(&value, value_size);
+	return value;
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 std::int16_t Archiver::read_int16()
-try {
-	return read_integer<std::int16_t>();
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	return static_cast<std::int16_t>(read_uint16());
+}
 
 std::uint16_t Archiver::read_uint16()
 try {
-	return read_integer<std::uint16_t>();
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 2;
+	std::byte bytes[value_size];
+	stream_->read_exactly(bytes, value_size);
+	crc32_.update(bytes, value_size);
+	return endian::read_u16_le(bytes);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 std::int32_t Archiver::read_int32()
-try {
-	return read_integer<std::int32_t>();
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	return static_cast<std::int32_t>(read_uint32());
+}
 
 std::uint32_t Archiver::read_uint32()
 try {
-	return read_integer<std::uint32_t>();
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 4;
+	std::byte bytes[value_size];
+	stream_->read_exactly(bytes, value_size);
+	crc32_.update(bytes, value_size);
+	return endian::read_u32_le(bytes);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::read_char_array(char* items, std::intptr_t item_count)
+void Archiver::read_char_array(char* items, int item_count)
 try {
-	read_integer_array(items, item_count);
+	read_uint8_array(reinterpret_cast<std::uint8_t*>(items), item_count);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::read_int8_array(std::int8_t* items_int8, std::intptr_t item_count)
+void Archiver::read_int8_array(std::int8_t* items, int item_count)
+{
+	read_uint8_array(reinterpret_cast<std::uint8_t*>(items), item_count);
+}
+
+void Archiver::read_uint8_array(std::uint8_t* items, int item_count)
 try {
-	read_integer_array(items_int8, item_count);
+	BSTONE_ASSERT(is_open());
+	BSTONE_ASSERT(items != nullptr);
+	BSTONE_ASSERT(item_count > 0);
+	stream_->read_exactly(items, item_count);
+	crc32_.update(items, item_count);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::read_uint8_array(std::uint8_t* items_uint8, std::intptr_t item_count)
+void Archiver::read_uint16_array(std::uint16_t* items, int item_count)
 try {
-	read_integer_array(items_uint8, item_count);
+	constexpr int item_size = 2;
+	const int items_size = item_count * item_size;
+	constexpr int max_buffer_size = 128;
+	constexpr int items_per_buffer = max_buffer_size / item_size;
+	std::byte buffer[max_buffer_size];
+	int item_offset = 0;
+	while (item_count > 0)
+	{
+		const int to_read_count = std::min(item_count, items_per_buffer);
+		const int to_read_size = to_read_count * item_size;
+		stream_->read_exactly(buffer, to_read_size);
+		crc32_.update(buffer, to_read_size);
+		for (int i = 0; i < to_read_count; ++i)
+		{
+			items[item_offset + i] = endian::read_u16_le(buffer + i * item_size);
+		}
+		item_count -= to_read_count;
+		item_offset += to_read_count;
+	}
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::read_uint16_array(std::uint16_t* items_uint16, std::intptr_t item_count)
-try {
-	read_integer_array(items_uint16, item_count);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-std::intptr_t Archiver::read_string(std::intptr_t max_string_length, char* string)
+int Archiver::read_string(int max_string_length, char* string)
 try {
 	BSTONE_ASSERT(max_string_length >= 0 && max_string_length <= INT32_MAX);
 	BSTONE_ASSERT(string != nullptr);
-
-	const auto archived_string_length = read_integer<std::int32_t>();
-
+	const std::int32_t archived_string_length = read_int32();
 	if (archived_string_length < 0 || archived_string_length > max_string_length)
 	{
 		BSTONE_THROW_STATIC_SOURCE("Archived string length out of range.");
 	}
-
 	stream_->read_exactly(string, archived_string_length);
 	return archived_string_length;
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 void Archiver::read_checksum()
 try {
-	const auto checksum = read_integer<std::uint32_t>(true);
-
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 4;
+	std::byte bytes[value_size];
+	stream_->read_exactly(bytes, value_size);
+	const std::uint32_t checksum = endian::read_u32_le(bytes);
 	if (checksum != crc32_.get_value())
 	{
 		BSTONE_THROW_STATIC_SOURCE("Checksum mismatch.");
@@ -108,73 +150,110 @@ try {
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 void Archiver::write_bool(bool value)
-try {
-	write_integer<std::uint8_t>(value);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	write_uint8(value);
+}
 
 void Archiver::write_int8(std::int8_t value)
-try {
-	write_integer(value);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	write_uint8(static_cast<std::uint8_t>(value));
+}
 
 void Archiver::write_uint8(std::uint8_t value)
 try {
-	write_integer(value);
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 1;
+	crc32_.update(&value, value_size);
+	stream_->write_exactly(&value, value_size);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 void Archiver::write_int16(std::int16_t value)
-try {
-	write_integer(value);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	write_uint16(static_cast<std::uint16_t>(value));
+}
 
 void Archiver::write_uint16(std::uint16_t value)
 try {
-	write_integer(value);
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 2;
+	std::byte bytes[value_size];
+	endian::write_u16_le(value, bytes);
+	crc32_.update(bytes, value_size);
+	stream_->write_exactly(bytes, value_size);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 void Archiver::write_int32(std::int32_t value)
-try {
-	write_integer(value);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
+{
+	write_uint32(static_cast<std::uint32_t>(value));
+}
 
 void Archiver::write_uint32(std::uint32_t value)
 try {
-	write_integer(value);
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 4;
+	std::byte bytes[value_size];
+	endian::write_u32_le(value, bytes);
+	crc32_.update(bytes, value_size);
+	stream_->write_exactly(bytes, value_size);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::write_char_array(const char* items_char, std::intptr_t item_count)
+void Archiver::write_char_array(const char* items, int item_count)
 try {
-	write_integer_array(items_char, item_count);
+	write_uint8_array(reinterpret_cast<const std::uint8_t*>(items), item_count);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::write_int8_array(const std::int8_t* items_int8, std::intptr_t item_count)
+void Archiver::write_int8_array(const std::int8_t* items, int item_count)
+{
+	write_uint8_array(reinterpret_cast<const std::uint8_t*>(items), item_count);
+}
+
+void Archiver::write_uint8_array(const std::uint8_t* items, int item_count)
 try {
-	write_integer_array(items_int8, item_count);
+	BSTONE_ASSERT(is_open());
+	BSTONE_ASSERT(items != nullptr);
+	BSTONE_ASSERT(item_count > 0);
+	crc32_.update(items, item_count);
+	stream_->write_exactly(items, item_count);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::write_uint8_array(const std::uint8_t* items_uint8, std::intptr_t item_count)
+void Archiver::write_uint16_array(const std::uint16_t* items, int item_count)
 try {
-	write_integer_array(items_uint8, item_count);
+	constexpr int item_size = 2;
+	const int items_size = item_count * item_size;
+	constexpr int max_buffer_size = 128;
+	constexpr int items_per_buffer = max_buffer_size / item_size;
+	std::byte buffer[max_buffer_size];
+	int item_offset = 0;
+	while (item_count > 0)
+	{
+		const int to_write_count = std::min(item_count, items_per_buffer);
+		for (int i = 0; i < to_write_count; ++i)
+		{
+			endian::write_u16_le(items[item_offset + i], buffer + i * item_size);
+		}
+		const int to_write_size = to_write_count * item_size;
+		crc32_.update(buffer, to_write_size);
+		stream_->write_exactly(buffer, to_write_size);
+		item_count -= to_write_count;
+		item_offset += to_write_count;
+	}
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
-void Archiver::write_uint16_array(const std::uint16_t* items_uint16, std::intptr_t item_count)
-try {
-	write_integer_array(items_uint16, item_count);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-void Archiver::write_string(const char* string, std::intptr_t string_length)
+void Archiver::write_string(const char* string, int string_length)
 try {
 	BSTONE_ASSERT(string != nullptr);
 	BSTONE_ASSERT(string_length >= 0 && string_length <= INT32_MAX);
-
-	write_integer<std::int32_t>(static_cast<std::int32_t>(string_length));
+	write_int32(static_cast<std::int32_t>(string_length));
 	stream_->write_exactly(string, string_length);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 void Archiver::write_checksum()
 try {
-	const auto checksum = crc32_.get_value();
-	write_integer<std::int32_t>(checksum, true);
+	BSTONE_ASSERT(is_open());
+	constexpr int value_size = 4;
+	std::byte bytes[value_size];
+	endian::write_u32_le(crc32_.get_value(), bytes);
+	stream_->write_exactly(bytes, value_size);
 } BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 } // namespace bstone
--- src/bstone/src/bstone_archiver.h
+++ src/bstone/src/bstone_archiver.h
@@ -7,16 +7,9 @@ SPDX-License-Identifier: MIT
 #ifndef BSTONE_ARCHIVER_INCLUDED
 #define BSTONE_ARCHIVER_INCLUDED
 
-#include <cstdint>
-
-#include <algorithm>
-#include <type_traits>
-
-#include "bstone_assert.h"
 #include "bstone_crc32.h"
-#include "bstone_endian.h"
-#include "bstone_exception.h"
 #include "bstone_stream.h"
+#include <cstdint>
 
 namespace bstone {
 
@@ -24,8 +17,8 @@ class Archiver
 {
 public:
 	void open(Stream& stream);
-	void close() noexcept;
-	bool is_open() const noexcept;
+	void close();
+	bool is_open() const;
 
 	bool read_bool();
 	std::int8_t read_int8();
@@ -34,173 +27,31 @@ public:
 	std::uint16_t read_uint16();
 	std::int32_t read_int32();
 	std::uint32_t read_uint32();
-
-	void read_char_array(char* items_char, std::intptr_t item_count);
-	void read_int8_array(std::int8_t* items_int8, std::intptr_t item_count);
-	void read_uint8_array(std::uint8_t* items_uint8, std::intptr_t item_count);
-	void read_uint16_array(std::uint16_t* items_uint16, std::intptr_t item_count);
-
-	std::intptr_t read_string(std::intptr_t max_string_length, char* string);
-
+	void read_char_array(char* items, int item_count);
+	void read_int8_array(std::int8_t* items, int item_count);
+	void read_uint8_array(std::uint8_t* items, int item_count);
+	void read_uint16_array(std::uint16_t* items, int item_count);
+	int read_string(int max_string_length, char* string);
 	void read_checksum();
 
-	void write_bool(bool value_bool);
-	void write_int8(std::int8_t value_int8);
-	void write_uint8(std::uint8_t value_uint8);
-	void write_int16(std::int16_t value_int16);
-	void write_uint16(std::uint16_t value_uint16);
-	void write_int32(std::int32_t value_int32);
-	void write_uint32(std::uint32_t value_uint32);
-
-	void write_char_array(const char* items_char, std::intptr_t item_count);
-	void write_int8_array(const std::int8_t* items_int8, std::intptr_t item_count);
-	void write_uint8_array(const std::uint8_t* items_uint8, std::intptr_t item_count);
-	void write_uint16_array(const std::uint16_t* items_uint16, std::intptr_t item_count);
-
-	void write_string(const char* string, std::intptr_t string_length);
-
+	void write_bool(bool value);
+	void write_int8(std::int8_t value);
+	void write_uint8(std::uint8_t value);
+	void write_int16(std::int16_t value);
+	void write_uint16(std::uint16_t value);
+	void write_int32(std::int32_t value);
+	void write_uint32(std::uint32_t value);
+	void write_char_array(const char* items, int item_count);
+	void write_int8_array(const std::int8_t* items, int item_count);
+	void write_uint8_array(const std::uint8_t* items, int item_count);
+	void write_uint16_array(const std::uint16_t* items, int item_count);
+	void write_string(const char* string, int string_length);
 	void write_checksum();
-
-private:
-	struct EndianByteSizeTag {};
-	struct EndianNonByteSizeTag {};
 
 private:
 	Crc32 crc32_{};
 	Stream* stream_{};
-
-private:
-	template<typename T>
-	T read_integer(bool is_checksum = false);
-
-	template<typename T>
-	void write_integer(T integer_value, bool is_checksum = false);
-
-	template<typename T>
-	void read_integer_array(T* items, std::intptr_t item_count);
-
-	template<typename T>
-	void write_integer_array_internal(const T* items, std::intptr_t item_count, EndianByteSizeTag);
-
-	template<typename T>
-	void write_integer_array_internal(const T* items, std::intptr_t item_count, EndianNonByteSizeTag);
-
-	template<typename T>
-	void write_integer_array(const T* items, std::intptr_t item_count);
 };
-
-// --------------------------------------------------------------------------
-
-template<typename T>
-T Archiver::read_integer(bool is_checksum)
-try {
-	BSTONE_ASSERT(is_open());
-
-	constexpr auto value_size = static_cast<std::intptr_t>(sizeof(T));
-	auto value = T{};
-
-	stream_->read_exactly(&value, value_size);
-
-	if (!is_checksum)
-	{
-		crc32_.update(&value, value_size);
-	}
-
-	return endian::to_little(value);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-template<typename T>
-void Archiver::write_integer(T integer_value, bool is_checksum)
-try {
-	BSTONE_ASSERT(is_open());
-
-	constexpr auto value_size = static_cast<std::intptr_t>(sizeof(T));
-
-	if (!is_checksum)
-	{
-		crc32_.update(&integer_value, value_size);
-	}
-
-	const auto value = endian::to_little(integer_value);
-	stream_->write_exactly(&value, value_size);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-template<typename T>
-void Archiver::read_integer_array(T* items, std::intptr_t item_count)
-try {
-	BSTONE_ASSERT(is_open());
-	BSTONE_ASSERT(items != nullptr);
-	BSTONE_ASSERT(item_count > 0);
-
-	constexpr auto value_size = static_cast<std::intptr_t>(sizeof(T));
-	const auto items_size = value_size * item_count;
-	stream_->read_exactly(items, items_size);
-
-#if BSTONE_ENDIAN == BSTONE_BIG_ENDIAN
-	for (auto i_item = std::intptr_t{}; i_item < item_count; ++i_item)
-	{
-		items[i_item] = endian::swap_bytes(items[i_item]);
-	}
-#endif
-
-	crc32_.update(items, items_size);
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
-
-template<typename T>
-void Archiver::write_integer_array_internal(const T* items, std::intptr_t item_count, EndianByteSizeTag)
-{
-	crc32_.update(items, item_count);
-	stream_->write_exactly(items, item_count);
-}
-
-template<typename T>
-void Archiver::write_integer_array_internal(const T* items, std::intptr_t item_count, EndianNonByteSizeTag)
-{
-	constexpr auto item_size = static_cast<std::intptr_t>(sizeof(T));
-	const auto items_size = item_size * item_count;
-
-	crc32_.update(items, items_size);
-
-	constexpr auto max_buffer_size = 1024;
-	constexpr auto items_per_buffer = max_buffer_size / item_size;
-	T buffer[items_per_buffer];
-	auto item_offset = std::intptr_t{};
-
-	while (item_count > 0)
-	{
-		const auto to_write_count = std::min(item_count, items_per_buffer);
-
-		std::transform(
-			&items[item_offset],
-			&items[item_offset + to_write_count],
-			buffer,
-			endian::to_little<T>);
-
-		const auto to_write_size = item_size * to_write_count;
-		stream_->write_exactly(buffer, to_write_size);
-
-		item_count -= to_write_count;
-		item_offset += to_write_count;
-	}
-}
-
-template<typename T>
-void Archiver::write_integer_array(const T* items, std::intptr_t item_count)
-try {
-	BSTONE_ASSERT(is_open());
-	BSTONE_ASSERT(items != nullptr);
-	BSTONE_ASSERT(item_count > 0);
-
-#if BSTONE_ENDIAN == BSTONE_BIG_ENDIAN
-	using Tag = std::conditional_t<sizeof(T) == 1, EndianByteSizeTag, EndianNonByteSizeTag>;
-	write_integer_array_internal(items, item_count, Tag{});
-#else
-	constexpr auto item_size = static_cast<std::intptr_t>(sizeof(T));
-	const auto items_size = item_size * item_count;
-	crc32_.update(items, items_size);
-	stream_->write_exactly(items, items_size);
-#endif
-} BSTONE_END_FUNC_CATCH_ALL_THROW_NESTED
 
 } // namespace bstone
 
