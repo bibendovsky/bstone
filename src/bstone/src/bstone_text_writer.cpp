8871d887bad149facfe77151e8ab21c8186eea18
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-17 20:52:33 +0200

    Revise bstone_text_writer

--- src/bstone/src/bstone_text_writer.cpp
+++ src/bstone/src/bstone_text_writer.cpp
@@ -4,54 +4,24 @@ Copyright (c) 2013-2024 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contrib
 SPDX-License-Identifier: MIT
 */
 
-
-//
-// A text writer for a stream.
-//
-
+// A text writer for a stream
 
 #include "bstone_text_writer.h"
 
+namespace bstone {
 
-namespace bstone
-{
-
-
-TextWriter::TextWriter()
-	:
-	stream_{}
-{
-}
-
-TextWriter::TextWriter(
-	Stream* stream)
+TextWriter::TextWriter(Stream* stream)
 	:
 	TextWriter{}
 {
-	static_cast<void>(open(stream));
+	open(stream);
 }
 
-TextWriter::TextWriter(
-	TextWriter&& rhs) noexcept
-	:
-	stream_{std::move(rhs.stream_)}
-{
-	rhs.stream_ = nullptr;
-}
-
-bool TextWriter::open(
-	Stream* stream)
+bool TextWriter::open(Stream* stream)
 {
 	close();
-
-	if (!stream)
-	{
-		return false;
-	}
-
 	stream_ = stream;
-
-	return true;
+	return is_open();
 }
 
 void TextWriter::close()
@@ -59,27 +29,23 @@ void TextWriter::close()
 	stream_ = nullptr;
 }
 
-bool TextWriter::is_initialized() const
+bool TextWriter::is_open() const
 {
 	return stream_ != nullptr;
 }
 
-bool TextWriter::write(
-	const std::string& string)
+bool TextWriter::write(const std::string& string)
 {
-	if (!is_initialized())
+	if (!is_open())
 	{
 		return false;
 	}
-
 	if (string.empty())
 	{
 		return true;
 	}
-
 	const auto to_write_size = static_cast<std::intptr_t>(string.length());
 	return stream_->write(string.data(), to_write_size) == to_write_size;
 }
 
-
-} // bstone
+} // namespace bstone
