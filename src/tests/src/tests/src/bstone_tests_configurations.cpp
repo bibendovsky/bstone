a71c069ac73d72c1bdad0ad3d40f761593f7faf2
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-03-14 18:19:52 +0200

    Remove bstone_tests_configurations.cpp

--- src/tests/src/tests/src/bstone_tests_configurations.cpp
+++
@@ -1,8 +0,0 @@
-#include "bstone_configurations.h"
-
-namespace bstone {
-
-extern const std::intptr_t posix_string_capacity = 256 * 1024;
-extern const std::intptr_t win32_wstring_capacity = 256 * 1024;
-
-} // namespace bstone
